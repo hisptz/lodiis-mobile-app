2601505a0a77d1e7f9561ea794a7289274008e70
Joseph Chingalo <profschinfalo@gmail.com>
2020-08-19 15:47:47 +0300

    Add icons and styles for login page

--- lib/pages/login_page/constants/login_style.dart
+++ lib/pages/login_page/constants/login_style.dart
@@ -6,4 +6,10 @@ class LoginPageStyles {
     fontWeight: FontWeight.normal,
     color: const Color(0xFF4B9F46),
   );
+
+  static TextStyle formInputValueStyle = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: const Color(0xFF1A3518),
+  );
 }
