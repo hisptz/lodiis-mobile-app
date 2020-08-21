ee49c47ead1e8fe9018e782d70451993b09e97bb
Joseph Chingalo <profschinfalo@gmail.com>
2020-08-21 09:46:23 +0300

    Add App bar for interventions

--- lib/modules/dreams_intervention/dreams_intervention.dart
+++ lib/modules/dreams_intervention/dreams_intervention.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/core/components/intervention_app_bar.dart';
 import 'package:kb_mobile_app/models/intervention_card.dart';
 import 'package:provider/provider.dart';
 import 'package:kb_mobile_app/app-state/intervention_card/intervention_card_state.dart';
 
 class DreamsIntervention extends StatelessWidget {
   const DreamsIntervention({Key key}) : super(key: key);
+
+  void onClickHome() {
+    print('on hoemm');
+  }
+
+  void onSearch() {
+    print('on hoemm');
+  }
+
+  void onAddHouseHold() {
+    print('on hoemm');
+  }
+
+  void onOpenMoreMenu() {
+    print('on hoemm');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +32,16 @@ class DreamsIntervention extends StatelessWidget {
         intervetionCardState.currentIntervetionProgram;
 
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(65),
+        child: InterventionAppBar(
+          activeInterventionProgram: activeInterventionProgram,
+          onClickHome: onClickHome,
+          onSearch: onSearch,
+          onAddHouseHold: onAddHouseHold,
+          onOpenMoreMenu: onOpenMoreMenu,
+        ),
+      ),
       body: SafeArea(
           child: Container(
         child: Text(
--- lib/modules/ovc_intervention/ovc_intervention.dart
+++ lib/modules/ovc_intervention/ovc_intervention.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:kb_mobile_app/app-state/intervention_card/intervention_card_state.dart';
+import 'package:kb_mobile_app/core/components/intervention_app_bar.dart';
 import 'package:kb_mobile_app/models/intervention_card.dart';
 import 'package:provider/provider.dart';
 
 class OvcIntervention extends StatelessWidget {
   const OvcIntervention({Key key}) : super(key: key);
+
+  void onClickHome() {
+    print('on hoemm');
+  }
+
+  void onSearch() {
+    print('on hoemm');
+  }
+
+  void onAddHouseHold() {
+    print('on hoemm');
+  }
+
+  void onOpenMoreMenu() {
+    print('on hoemm');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +32,31 @@ class OvcIntervention extends StatelessWidget {
         intervetionCardState.currentIntervetionProgram;
 
     return Scaffold(
-      body: SafeArea(
-          child: Container(
-        child: Text('Ovc intervetion - ${activeInterventionProgram.name}'),
-      )),
-    );
+        body: SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(65),
+          child: InterventionAppBar(
+            activeInterventionProgram: activeInterventionProgram,
+            onClickHome: onClickHome,
+            onSearch: onSearch,
+            onAddHouseHold: onAddHouseHold,
+            onOpenMoreMenu: onOpenMoreMenu,
+          ),
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Container(
+              decoration:
+                  BoxDecoration(color: activeInterventionProgram.background),
+            ),
+            Container(
+              child: Text('container for ${activeInterventionProgram.name}'),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
