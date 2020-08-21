745957c8d0c22f4f9dede4adf633e3f0d0759de7
Joseph Chingalo <profschinfalo@gmail.com>
2020-08-21 16:00:12 +0300

    add state management for nav buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/app-state/intervention_bottom_navigation_state/intervention_bottom_navigation_state.dart';
 import 'package:kb_mobile_app/app-state/intervention_card_state/intervention_card_state.dart';
-import 'package:kb_mobile_app/app-state/page_state/page_state.dart';
 import 'package:kb_mobile_app/core/constants/custom_color.dart';
 import 'package:kb_mobile_app/modules/splash/splash_page.dart';
 import 'package:provider/provider.dart';
@@ -14,8 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (_) => PageState()),
-          ChangeNotifierProvider(create: (_) => IntervetionCardState())
+          ChangeNotifierProvider(create: (_) => IntervetionCardState()),
+          ChangeNotifierProvider(
+              create: (_) => InterventionBottomNavigationState())
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
