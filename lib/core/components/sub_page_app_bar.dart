beac6cced09c0c9d9e4201ef30fdeabdc3568d3f
Joseph Chingalo <profschinfalo@gmail.com>
2020-09-11 09:55:45 +0300

    Refactor control for app bar

--- lib/core/components/sub_page_app_bar.dart
+++ lib/core/components/sub_page_app_bar.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kb_mobile_app/app_state/intervention_bottom_navigation_state/intervention_bottom_navigation_state.dart';
-import 'package:kb_mobile_app/app_state/intervention_card_state/intervention_card_state.dart';
-import 'package:kb_mobile_app/core/components/intervention_pop_up_menu.dart';
-import 'package:kb_mobile_app/core/services/user_service.dart';
-import 'package:kb_mobile_app/core/utils/app_util.dart';
+import 'package:kb_mobile_app/core/utils/app_bar_util.dart';
 import 'package:kb_mobile_app/models/intervention_card.dart';
-import 'package:kb_mobile_app/modules/dreams_intervention/dreams_intervention.dart';
-import 'package:kb_mobile_app/modules/login/login.dart';
-import 'package:kb_mobile_app/modules/ovc_intervention/ovc_intervention.dart';
-import 'package:provider/provider.dart';
 
 class SubPageAppBar extends StatelessWidget {
   const SubPageAppBar({
@@ -20,44 +12,13 @@ class SubPageAppBar extends StatelessWidget {
   final String label;
   final InterventionCard activeInterventionProgram;
 
+  void onOpenMoreMenu(
+      BuildContext context, InterventionCard activeInterventionProgram) async {
+    AppBarUtil.onOpenMoreMenu(context, activeInterventionProgram);
+  }
+
   @override
   Widget build(BuildContext context) {
-// state controllers
-    IntervetionCardState intervetionCardState =
-        Provider.of<IntervetionCardState>(context, listen: false);
-    InterventionBottomNavigationState interventionBottomNavigationState =
-        Provider.of<InterventionBottomNavigationState>(context, listen: false);
-
-    void onLogOut() async {
-      await UserService().logout();
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => Login()));
-    }
-
-    void onSwitchToIntervention(String id) {
-      intervetionCardState.setCurrentInterventionProgramId(id);
-      interventionBottomNavigationState
-          .setCurrentInterventionBottomNavigationIndex(0);
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-              builder: (context) =>
-                  id == 'ovc' ? OvcIntervention() : DreamsIntervention()));
-    }
-
-    void onOpenMoreMenu(InterventionCard activeInterventionProgram) async {
-      var modal = InterventionPopUpMenu(
-          activeInterventionProgram: activeInterventionProgram);
-      var response = await AppUtil.showPopUpModal(context, modal);
-      if (response != null) {
-        if (response.id == 'dreams' || response.id == 'ovc') {
-          onSwitchToIntervention(response.id);
-        } else if (response.id == 'logout') {
-          onLogOut();
-        }
-      }
-    }
-
     return AppBar(
       title: Text(
         label,
@@ -69,7 +30,8 @@ class SubPageAppBar extends StatelessWidget {
         Container(
           child: IconButton(
               icon: Icon(Icons.more_vert),
-              onPressed: () => onOpenMoreMenu(activeInterventionProgram)),
+              onPressed: () =>
+                  onOpenMoreMenu(context, activeInterventionProgram)),
         )
       ],
     );
--- lib/modules/dreams_intervention/dreams_intervention.dart
+++ lib/modules/dreams_intervention/dreams_intervention.dart
@@ -3,10 +3,8 @@ import 'package:kb_mobile_app/app_state/intervention_bottom_navigation_state/int
 import 'package:kb_mobile_app/app_state/intervention_card_state/intervention_card_state.dart';
 import 'package:kb_mobile_app/core/components/Intervention_bottom_navigation_bar.dart';
 import 'package:kb_mobile_app/core/components/intervention_app_bar.dart';
-import 'package:kb_mobile_app/core/components/intervention_pop_up_menu.dart';
 import 'package:kb_mobile_app/core/components/route_page_not_found.dart';
-import 'package:kb_mobile_app/core/services/user_service.dart';
-import 'package:kb_mobile_app/core/utils/app_util.dart';
+import 'package:kb_mobile_app/core/utils/app_bar_util.dart';
 import 'package:kb_mobile_app/models/Intervention_bottom_navigation.dart';
 import 'package:kb_mobile_app/models/intervention_card.dart';
 import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_enrollment/dreams_enrollment_page.dart';
@@ -15,68 +13,38 @@ import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_exit
 import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_referral/dreams_referral_page.dart';
 import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_services/dreams_services_page.dart';
 import 'package:kb_mobile_app/modules/dreams_intervention/submodules/none_agyw/none_agyw_page.dart';
-import 'package:kb_mobile_app/modules/login/login.dart';
-import 'package:kb_mobile_app/modules/ovc_intervention/ovc_intervention.dart';
 import 'package:provider/provider.dart';
 
 class DreamsIntervention extends StatelessWidget {
   const DreamsIntervention({Key key}) : super(key: key);
 
+  void onOpenMoreMenu(
+      BuildContext context, InterventionCard activeInterventionProgram) async {
+    AppBarUtil.onOpenMoreMenu(context, activeInterventionProgram);
+  }
+
+  void onClickHome() {
+    print('on onClickHome');
+  }
+
+  void onSearch() {
+    print('on onSearch');
+  }
+
+  void onAddNoneAgywBeneficiary() {
+    print('onAddNoneAgywBeneficiary');
+  }
+
+  void onAddAgywBeneficiary(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context) {
+        return DreamsEnrollmentForm();
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    // state controllers
-    IntervetionCardState intervetionCardState =
-        Provider.of<IntervetionCardState>(context, listen: false);
-    InterventionBottomNavigationState interventionBottomNavigationState =
-        Provider.of<InterventionBottomNavigationState>(context, listen: false);
-
-    void onLogOut() async {
-      await UserService().logout();
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => Login()));
-    }
-
-    void onSwitchToOvc(String id) {
-      intervetionCardState.setCurrentInterventionProgramId(id);
-      interventionBottomNavigationState
-          .setCurrentInterventionBottomNavigationIndex(0);
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => OvcIntervention()));
-    }
-
-    void onClickHome() {
-      print('on onClickHome');
-    }
-
-    void onSearch() {
-      print('on onSearch');
-    }
-
-    void onAddNoneAgywBeneficiary() {
-      print('onAddNoneAgywBeneficiary');
-    }
-
-    void onAddAgywBeneficiary() {
-      Navigator.push(context, MaterialPageRoute(
-        builder: (context) {
-          return DreamsEnrollmentForm();
-        },
-      ));
-    }
-
-    void onOpenMoreMenu(InterventionCard activeInterventionProgram) async {
-      var modal = InterventionPopUpMenu(
-          activeInterventionProgram: activeInterventionProgram);
-      var response = await AppUtil.showPopUpModal(context, modal);
-      if (response != null) {
-        if (response.id == 'ovc') {
-          onSwitchToOvc(response.id);
-        } else if (response.id == 'logout') {
-          onLogOut();
-        }
-      }
-    }
-
     return SafeArea(
       child: Scaffold(
         appBar: PreferredSize(
@@ -89,9 +57,10 @@ class DreamsIntervention extends StatelessWidget {
                 activeInterventionProgram: activeInterventionProgram,
                 onClickHome: onClickHome,
                 onSearch: onSearch,
-                onAddAgywBeneficiary: onAddAgywBeneficiary,
+                onAddAgywBeneficiary: () => onAddAgywBeneficiary(context),
                 onAddNoneAgywBeneficiary: onAddNoneAgywBeneficiary,
-                onOpenMoreMenu: () => onOpenMoreMenu(activeInterventionProgram),
+                onOpenMoreMenu: () =>
+                    onOpenMoreMenu(context, activeInterventionProgram),
               );
             },
           ),
@@ -140,7 +109,7 @@ class DreamsIntervention extends StatelessWidget {
           );
         }),
         bottomNavigationBar: Consumer<IntervetionCardState>(
-          builder: (context, value, child) {
+          builder: (context, intervetionCardState, child) {
             InterventionCard activeInterventionProgram =
                 intervetionCardState.currentIntervetionProgram;
             return InterventionBottomNavigationBar(
