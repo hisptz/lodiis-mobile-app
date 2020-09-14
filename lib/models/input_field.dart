56240275e9dd8292b31858209e45daac466938ce
Joseph Chingalo <profschingalo@gmail.com>
2020-09-14 18:30:35 +0300

    Add model for input fields

---
+++ lib/models/input_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/models/input_field_option.dart';
+
+class InputField {
+  String id;
+  String name;
+  String description;
+  String valueType;
+  String value;
+  Color color;
+  Color background;
+  List<InputFieldOption> options;
+  bool hasSubInputField;
+  InputField subInputField;
+
+  InputField(
+      {@required this.id,
+      @required this.name,
+      @required this.valueType,
+      this.hasSubInputField,
+      this.description,
+      this.value,
+      this.color,
+      this.background,
+      this.options,
+      this.subInputField}) {
+    this.description = this.description ?? '';
+    this.hasSubInputField = this.hasSubInputField ?? false;
+    this.value = this.value ?? '';
+    this.options = this.options ?? [];
+    this.color = this.color ?? Colors.black;
+    this.background = this.background ?? Colors.transparent;
+    this.subInputField = this.subInputField ?? null;
+  }
+
+  @override
+  String toString() {
+    return '$id - $name';
+  }
+}
