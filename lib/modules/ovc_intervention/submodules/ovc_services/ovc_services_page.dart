b341bf2fdb5f713c893bcdf836eb7d3be6584e09
Joseph Chingalo <profschingalo@gmail.com>
2020-09-21 18:45:59 +0300

    chore: remove unused codes

--- lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_page.dart
+++ lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:kb_mobile_app/modules/ovc_intervention/components/ovcIntervention_home-card.dart';
 import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_intervention_appBar.dart';
-import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_intervention_children.dart';
 import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_intervention_home_card.dart';
-import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_intervention_home_card_body.dart';
-
 
 class OvcServicesPage extends StatelessWidget {
   const OvcServicesPage({Key key}) : super(key: key);
-
 
   void onAssess() {
     print("on ASSESS");
@@ -22,35 +17,50 @@ class OvcServicesPage extends StatelessWidget {
     print("on Monitor");
   }
 
-
   @override
   Widget build(BuildContext context) {
-    bool editService = true;
     return Scaffold(
       backgroundColor: Color(0xffedf4ed),
-      appBar:AppBar(
-         backgroundColor: Colors.white,
-         title: OvcInterventionAppBar(title: "HOUSE HOLD LIST"),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: OvcInterventionAppBar(title: "HOUSE HOLD LIST"),
       ),
-        body:  SingleChildScrollView(
-          
-          
-                  child:
-                  
-                   Column(
-            children: [
-            OvcInterventionCard(editService: true, addExit: false, editEnrollment: false, editReferral: false,),
-            OvcInterventionCard(editService: true, addExit: false, editEnrollment: false, editReferral: false,),
-            OvcInterventionCard(editService: true, addExit: false, editEnrollment: false, editReferral: false,),
-            OvcInterventionCard(editService: true, addExit: false, editEnrollment: false, editReferral: false,),
-            OvcInterventionCard(editService: true, addExit: false, editEnrollment: false, editReferral: false,),
-            ],
-          
-      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            OvcInterventionCard(
+              editService: true,
+              addExit: false,
+              editEnrollment: false,
+              editReferral: false,
+            ),
+            OvcInterventionCard(
+              editService: true,
+              addExit: false,
+              editEnrollment: false,
+              editReferral: false,
+            ),
+            OvcInterventionCard(
+              editService: true,
+              addExit: false,
+              editEnrollment: false,
+              editReferral: false,
+            ),
+            OvcInterventionCard(
+              editService: true,
+              addExit: false,
+              editEnrollment: false,
+              editReferral: false,
+            ),
+            OvcInterventionCard(
+              editService: true,
+              addExit: false,
+              editEnrollment: false,
+              editReferral: false,
+            ),
+          ],
         ),
-      );
-    
-    
-    
+      ),
+    );
   }
 }
