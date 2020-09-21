de74af829b3713f8192b7fb6cbac94a8d45bea76
Joseph Chingalo <profschingalo@gmail.com>
2020-09-21 17:44:49 +0300

    fix: add statues for enrollment forms

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:kb_mobile_app/app_state/enrollment_form_state/enrollment_form_state.dart';
+import 'package:kb_mobile_app/app_state/enrollment_service_form_state/enrollment_form_state.dart';
 import 'package:kb_mobile_app/app_state/intervention_bottom_navigation_state/intervention_bottom_navigation_state.dart';
 import 'package:kb_mobile_app/app_state/intervention_card_state/intervention_card_state.dart';
 import 'package:kb_mobile_app/app_state/login_form_state/login_form_state.dart';
--- lib/app_state/enrollment_form_state/enrollment_form_state.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-class EnrollmentFormState with ChangeNotifier {
-  // intial state
-
-  // selector
-
-  //reducers
-}
