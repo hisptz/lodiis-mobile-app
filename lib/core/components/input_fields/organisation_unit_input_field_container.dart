e9cf068250c7f5d8470e84f52f8c6edf336e4b15
Joseph Chingalo <profschingalo@gmail.com>
2020-09-21 09:31:58 +0300

    feat: set up ou input with empty modal\

--- lib/core/components/input_fields/organisation_unit_input_field_container.dart
+++ lib/core/components/input_fields/organisation_unit_input_field_container.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:kb_mobile_app/core/components/input_fields/input_checked_cion.dart';
-import 'package:kb_mobile_app/core/utils/organisation_uni_recursive.dart/organisation_popUpMenu.dart';
-import 'package:kb_mobile_app/core/utils/organisation_uni_recursive.dart/organisation_unit_recursive_util.dart';
+import 'package:kb_mobile_app/core/utils/app_util.dart';
 import 'package:kb_mobile_app/models/input_field.dart';
 
-class OrganisationUnitInputFieldContainer extends StatelessWidget {
+class OrganisationUnitInputFieldContainer extends StatefulWidget {
   const OrganisationUnitInputFieldContainer(
       {Key key, @required this.inputField, @required this.onInputValueChange})
       : super(key: key);
@@ -13,28 +12,52 @@ class OrganisationUnitInputFieldContainer extends StatelessWidget {
   final Function onInputValueChange;
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        child: Row(
-          children: [
-            Expanded(child: Text(inputField.valueType)),
-            InputCheckedIcon(
-              showTickedIcon: false,
-              color: inputField.background,
-            )
-          ],
-        ),
-      ),
-      onTap: () async {
-        var modal = OrganisationUnitPopUpMenu();
-        var response = await OrganisationUtil.showPopUpModal(context, modal);
+  _OrganisationUnitInputFieldContainerState createState() =>
+      _OrganisationUnitInputFieldContainerState();
+}
 
-        if (response != null) {
-          inputField.valueType = response.name;
-        }
-        (context as Element).markNeedsBuild();
-      },
+class _OrganisationUnitInputFieldContainerState
+    extends State<OrganisationUnitInputFieldContainer> {
+  TextEditingController organisationUnitController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void setOrganisationunit() {}
+
+  void openOrganisationUnit(BuildContext context) {
+    Widget modal = Container(
+      child: Text('Hello'),
+    );
+    var response = AppUtil.showPopUpModal(context, modal);
+    if (response != null) {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextFormField(
+                  controller: organisationUnitController,
+                  style:
+                      TextStyle().copyWith(color: widget.inputField.labelColor),
+                  onTap: () => openOrganisationUnit(context),
+                  readOnly: true,
+                  textInputAction: TextInputAction.done,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    errorText: null,
+                  ))),
+          InputCheckedIcon(
+            showTickedIcon: false,
+            color: widget.inputField.background,
+          )
+        ],
+      ),
     );
   }
 }
