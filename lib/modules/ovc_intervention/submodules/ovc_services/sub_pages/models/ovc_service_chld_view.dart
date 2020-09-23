9d678d9684d9c9ab328c784e692561d158a1b52c
Danford Kija <43994719+Kijacode@users.noreply.github.com>
2020-09-23 18:10:32 +0300

    update arrangement of ovc child service cards icons

--- lib/modules/ovc_intervention/submodules/ovc_services/sub_pages/models/ovc_service_chld_view.dart
+++ lib/modules/ovc_intervention/submodules/ovc_services/sub_pages/models/ovc_service_chld_view.dart
@@ -13,15 +13,15 @@ class OvcServiceChildModel {
     OvcServiceChildModel(
         iconPath: "assets/icons/child_case_plan_ovc_icon.svg",
         serviceNumber: 8,
-        title: "Case plan"),
-    OvcServiceChildModel(
-        iconPath: "assets/icons/child_monitor_ovc_icon.svg",
-        serviceNumber: 25,
-        title: "Monitor"),
+        title: "Case plan"),   
     OvcServiceChildModel(
         iconPath: "assets/icons/child_service_ovc_icon.svg",
         serviceNumber: 5,
         title: "Service"),
+     OvcServiceChildModel(
+        iconPath: "assets/icons/child_monitor_ovc_icon.svg",
+        serviceNumber: 25,
+        title: "Monitor"),
   ];
 
   @override
