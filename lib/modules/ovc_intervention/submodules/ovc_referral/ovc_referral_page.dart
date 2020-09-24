1f2990476819dedbc91dd3b151d161c8bf0b0db1
Kijacode <kijadanford@gmail.com>
2020-09-24 15:47:00 +0300

    set: on referral method to it correspond class

--- lib/modules/ovc_intervention/submodules/ovc_referral/ovc_referral_page.dart
+++ lib/modules/ovc_intervention/submodules/ovc_referral/ovc_referral_page.dart
@@ -1,53 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_intervention_appBar.dart';
 import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_intervention_home_card.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_referral/referral_pages/ovc_house_hold_referral.dart';
 
 class OvcReferralPage extends StatelessWidget {
   const OvcReferralPage({Key key}) : super(key: key);
 
-  Widget referral() {
-    return GestureDetector(
-      onTap: () {
-
-      },
-      child: Row(
-        
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            width: 20.25,
-          ),
-          SizedBox(
-            width: 13.65,
-          ),
-          Text("REFERRAL",
-              style: TextStyle(
-                color: Color(0xFF4B9F46)
-              )),
-        ],
-      ),
-    );
-  }
+  
 
   @override
   Widget build(BuildContext context) {
+    void onReferral() {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => OvcHouseHoldRefferal()));
+  }
     return Scaffold(
       backgroundColor: Color(0xffedf4ed),
-           appBar: AppBar(
-         backgroundColor: Colors.white,
-         title: OvcInterventionAppBar(title: "HOUSE HOLD LIST"),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: OvcInterventionAppBar(title: "HOUSE HOLD LIST"),
       ),
-      body:  SingleChildScrollView(
-              child: Column(
-            children: [
-             OvcInterventionCard(editReferral: true, addExit: false, editEnrollment: false, editService: false,),
-              OvcInterventionCard(editReferral: true, addExit: false, editEnrollment: false, editService: false,),
-               OvcInterventionCard(editReferral: true, addExit: false, editEnrollment: false, editService: false,),
-                OvcInterventionCard(editReferral: true, addExit: false, editEnrollment: false, editService: false,),
-                 OvcInterventionCard(editReferral: true, addExit: false, editEnrollment: false, editService: false,),
-            ],
-          
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            OvcInterventionCard(
+              onReferral: onReferral,
+              editReferral: true,
+              addExit: false,
+              editEnrollment: false,
+              editService: false,
+            ),
+            OvcInterventionCard(
+              onReferral: onReferral,
+              editReferral: true,
+              addExit: false,
+              editEnrollment: false,
+              editService: false,
+            ),
+            OvcInterventionCard(
+              onReferral: onReferral,
+              editReferral: true,
+              addExit: false,
+              editEnrollment: false,
+              editService: false,
+            ),
+            OvcInterventionCard(
+              onReferral: onReferral,
+              editReferral: true,
+              addExit: false,
+              editEnrollment: false,
+              editService: false,
+            ),
+            OvcInterventionCard(
+              onReferral: onReferral,
+              editReferral: true,
+              addExit: false,
+              editEnrollment: false,
+              editService: false,
+            ),
+          ],
         ),
       ),
     );
