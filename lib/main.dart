a16fd5687307bc4366e3d824a82b7f17e8706ffd
Kijacode <kijadanford@gmail.com>
2020-09-24 12:49:54 +0300

    add ovc assessment form container

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_stetho/flutter_stetho.dart';
 import 'package:kb_mobile_app/app.dart';
 
-void main() {
+void main() { 
+
   Stetho.initialize();
   runApp(MyApp());
 }
---
+++ lib/modules/ovc_intervention/submodules/ovc_services/sub_pages/asessment/pages/tb_new_assessment.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/app_state/intervention_card_state/intervention_card_state.dart';
+import 'package:kb_mobile_app/core/components/Intervention_bottom_navigation_bar_container.dart';
+import 'package:kb_mobile_app/core/components/sub_page_app_bar.dart';
+import 'package:kb_mobile_app/models/intervention_card.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_child_appbar_container.dart';
+import 'package:provider/provider.dart';
+
+
+
+class OvcServiceTBAssessment extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFFEDF4ED),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(65.0),
+          child: Consumer<IntervetionCardState>(
+            builder: (context, intervetionCardState, child) {
+              InterventionCard activeInterventionProgram =
+                  intervetionCardState.currentIntervetionProgram;
+              return SubPageAppBar(
+                label: "New TB Assessemnt",
+                activeInterventionProgram: activeInterventionProgram,
+              );
+            },
+          ),
+        ),
+        body: Column(children: [
+            OvcChildAppBarContainer(),
+           Container(
+             height: MediaQuery.of(context).size.height/3,
+             child: Center(
+               child: Text("TB Assessemnt forms"),
+             ),
+           ),
+          
+                        ]),
+                      
+                      bottomNavigationBar: InterventionBottomNavigationBarContainer());
+                }
+              
+               
+}
+
