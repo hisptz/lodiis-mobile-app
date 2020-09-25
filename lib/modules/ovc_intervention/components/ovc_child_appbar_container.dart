1ec356107590421ce93fa8362ecf6fe9d27af1ea
Joseph Chingalo <profschingalo@gmail.com>
2020-09-25 14:16:15 +0300

    chore: remove unused codes

--- lib/modules/ovc_intervention/components/ovc_child_appbar_container.dart
+++ lib/modules/ovc_intervention/components/ovc_child_appbar_container.dart
@@ -8,7 +8,8 @@ class OvcChildAppBarContainer extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 8),
       color: Color(0xFFFFFFFF),
       child: Column(
-        children: [ Row(
+        children: [
+          Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
@@ -16,26 +17,47 @@ class OvcChildAppBarContainer extends StatelessWidget {
                   child: Expanded(
                     child: Text("Pula Khabane",
                         style: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.w700)), )) ],  ),
-          LineSeperator(  height: 1, color: Color(0xFFECF9EB), ),
-          Container( color: Color(0xFFFFFFFF),
+                            fontSize: 14, fontWeight: FontWeight.w700)),
+                  ))
+            ],
+          ),
+          LineSeperator(
+            height: 1,
+            color: Color(0xFFECF9EB),
+          ),
+          Container(
+            color: Color(0xFFFFFFFF),
             padding: EdgeInsets.only(bottom: 9, top: 9),
-            child: Row(mainAxisAlignment: MainAxisAlignment.start,
-              children: [Expanded( flex: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                    flex: 0,
                     child: Container(
                         margin: EdgeInsets.only(left: 4),
-                        child: Row( mainAxisAlignment: MainAxisAlignment.start,
-                          children: [Text( "    Sex  ",style: TextStyle(color: Color(0xFF8FAF8F)),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              "    Sex  ",
+                              style: TextStyle(color: Color(0xFF8FAF8F)),
                             ),
                             Text("female      ",
-                                style: TextStyle(color: Color(0xFF35B638)))], ))),
-                Expanded( flex: 0, child: Container( child: Row(
+                                style: TextStyle(color: Color(0xFF35B638)))
+                          ],
+                        ))),
+                Expanded(
+                    flex: 0,
+                    child: Container(
+                        child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Text("Age  ",
                             style: TextStyle(color: Color(0xFF8FAF8F))),
                         Text("10       ",
-                            style: TextStyle(color: Color(0xFF35B638))) ], ))),
+                            style: TextStyle(color: Color(0xFF35B638)))
+                      ],
+                    ))),
                 Expanded(
                     flex: 0,
                     child: Container(
@@ -45,11 +67,14 @@ class OvcChildAppBarContainer extends StatelessWidget {
                         Text("Caregiver  ",
                             style: TextStyle(color: Color(0xFF8FAF8F))),
                         Text("Lenka Mosela",
-                            style: TextStyle(color: Color(0xFF35B638))) ], ))), ], ), ),  ],  ),);  
+                            style: TextStyle(color: Color(0xFF35B638)))
+                      ],
+                    ))),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
-    
-  
-      
-         
-           
