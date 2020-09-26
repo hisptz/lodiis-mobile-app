0bd72e34ca9a97fcceef7a7dfedb5e38aeff5ae8
Joseph Chingalo <profschingalo@gmail.com>
2020-09-26 13:29:37 +0300

    refactor: refactor ovc service home widget

--- lib/modules/ovc_intervention/components/ovc_house_hold_card_botton_content.dart
+++ lib/modules/ovc_intervention/components/ovc_house_hold_card_botton_content.dart
@@ -5,6 +5,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kb_mobile_app/core/components/line_seperator.dart';
 import 'package:kb_mobile_app/models/ovc_house_hold.dart';
 import 'package:kb_mobile_app/models/ovc_house_hold_child.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/sub_pages/ovc_child_service_home.dart';
 
 class OvcHouseHoldCardBottonContent extends StatelessWidget {
   const OvcHouseHoldCardBottonContent({
@@ -40,8 +41,13 @@ class OvcHouseHoldCardBottonContent extends StatelessWidget {
     print('onAddNewChild ');
   }
 
-  void onViewChildService(OvcHouseHoldChild child) {
-    print('onViewChildService ${child.toString()}');
+  void onViewChildService(BuildContext context, OvcHouseHoldChild child) {
+    //@TODO set current state for child and household
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OvcChildServiceHome(),
+        ));
   }
 
   void onViewChildReferral(OvcHouseHoldChild child) {
@@ -59,10 +65,13 @@ class OvcHouseHoldCardBottonContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 13.0, vertical: 10.0),
       child: Column(
         children: [
+          LineSeperator(
+            color: Color(0xFFECF5EC),
+          ),
           Container(
+            margin: EdgeInsets.symmetric(horizontal: 13.0, vertical: 10.0),
             child: Row(
               children: [
                 Container(
@@ -82,106 +91,109 @@ class OvcHouseHoldCardBottonContent extends StatelessWidget {
               ],
             ),
           ),
-          Column(
-            children: ovcHouseHold.children.map((OvcHouseHoldChild child) {
-              int index = ovcHouseHold.children.indexOf(child) + 1;
-              return Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      child: Text(
-                        '$index. ${child.toString()}',
-                        style: TextStyle().copyWith(
-                            fontSize: 14.0,
-                            color: Color(0xFF536852),
-                            fontWeight: FontWeight.w500),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 13.0, vertical: 10.0),
+            child: Column(
+              children: ovcHouseHold.children.map((OvcHouseHoldChild child) {
+                int index = ovcHouseHold.children.indexOf(child) + 1;
+                return Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Text(
+                          '$index. ${child.toString()}',
+                          style: TextStyle().copyWith(
+                              fontSize: 14.0,
+                              color: Color(0xFF536852),
+                              fontWeight: FontWeight.w500),
+                        ),
                       ),
                     ),
-                  ),
-                  Row(mainAxisSize: MainAxisSize.min, children: [
-                    Visibility(
-                      visible: canViewChildService ||
-                          canViewChildInfo ||
-                          canViewChildExit,
-                      child: Container(
-                          child: InkWell(
-                              onTap: () => canViewChildExit
-                                  ? onViewChildExit(child)
-                                  : canViewChildInfo
-                                      ? onViewChildInfo(child)
-                                      : canViewChildService
-                                          ? onViewChildService(child)
-                                          : null,
-                              child: Container(
-                                padding: EdgeInsets.all(10.0),
-                                child: Text(
-                                  'VIEW',
-                                  style: TextStyle().copyWith(
-                                    fontSize: 12.0,
-                                    color: Color(0xFF4B9F46),
-                                    fontWeight: FontWeight.w500,
+                    Row(mainAxisSize: MainAxisSize.min, children: [
+                      Visibility(
+                        visible: canViewChildService ||
+                            canViewChildInfo ||
+                            canViewChildExit,
+                        child: Container(
+                            child: InkWell(
+                                onTap: () => canViewChildExit
+                                    ? onViewChildExit(child)
+                                    : canViewChildInfo
+                                        ? onViewChildInfo(child)
+                                        : canViewChildService
+                                            ? onViewChildService(context, child)
+                                            : null,
+                                child: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Text(
+                                    'VIEW',
+                                    style: TextStyle().copyWith(
+                                      fontSize: 12.0,
+                                      color: Color(0xFF4B9F46),
+                                      fontWeight: FontWeight.w500,
+                                    ),
                                   ),
-                                ),
-                              ))),
-                    ),
-                    Visibility(
-                      visible: canAddChildExit,
-                      child: Container(
-                          margin: EdgeInsets.only(left: 10.0),
-                          child: InkWell(
-                              onTap: () => onAddChildExit(child),
-                              child: Container(
-                                padding: EdgeInsets.all(10.0),
-                                child: Text(
-                                  'ADD',
-                                  style: TextStyle().copyWith(
-                                    fontSize: 12.0,
-                                    color: Color(0xFF4B9F46),
-                                    fontWeight: FontWeight.w500,
+                                ))),
+                      ),
+                      Visibility(
+                        visible: canAddChildExit,
+                        child: Container(
+                            margin: EdgeInsets.only(left: 10.0),
+                            child: InkWell(
+                                onTap: () => onAddChildExit(child),
+                                child: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Text(
+                                    'ADD',
+                                    style: TextStyle().copyWith(
+                                      fontSize: 12.0,
+                                      color: Color(0xFF4B9F46),
+                                      fontWeight: FontWeight.w500,
+                                    ),
                                   ),
-                                ),
-                              ))),
-                    ),
-                    Visibility(
-                      visible: canViewChildReferral,
-                      child: Container(
-                          margin: EdgeInsets.only(left: 10.0),
-                          child: InkWell(
-                              onTap: () => onViewChildReferral(child),
-                              child: Container(
-                                padding: EdgeInsets.all(10.0),
-                                child: Text(
-                                  'REFERRAL',
-                                  style: TextStyle().copyWith(
-                                    fontSize: 12.0,
-                                    color: Color(0xFF4B9F46),
-                                    fontWeight: FontWeight.w500,
+                                ))),
+                      ),
+                      Visibility(
+                        visible: canViewChildReferral,
+                        child: Container(
+                            margin: EdgeInsets.only(left: 10.0),
+                            child: InkWell(
+                                onTap: () => onViewChildReferral(child),
+                                child: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Text(
+                                    'REFERRAL',
+                                    style: TextStyle().copyWith(
+                                      fontSize: 12.0,
+                                      color: Color(0xFF4B9F46),
+                                      fontWeight: FontWeight.w500,
+                                    ),
                                   ),
-                                ),
-                              ))),
-                    ),
-                    Visibility(
-                      visible: canEditChildInfo,
-                      child: Container(
-                          margin: EdgeInsets.only(left: 10.0),
-                          child: InkWell(
-                              onTap: () => onEditChildInfo(child),
-                              child: Container(
-                                padding: EdgeInsets.all(10.0),
-                                child: Text(
-                                  'EDIT',
-                                  style: TextStyle().copyWith(
-                                    fontSize: 12.0,
-                                    color: Color(0xFF4B9F46),
-                                    fontWeight: FontWeight.w500,
+                                ))),
+                      ),
+                      Visibility(
+                        visible: canEditChildInfo,
+                        child: Container(
+                            margin: EdgeInsets.only(left: 10.0),
+                            child: InkWell(
+                                onTap: () => onEditChildInfo(child),
+                                child: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Text(
+                                    'EDIT',
+                                    style: TextStyle().copyWith(
+                                      fontSize: 12.0,
+                                      color: Color(0xFF4B9F46),
+                                      fontWeight: FontWeight.w500,
+                                    ),
                                   ),
-                                ),
-                              ))),
-                    )
-                  ])
-                ],
-              );
-            }).toList(),
+                                ))),
+                      )
+                    ])
+                  ],
+                );
+              }).toList(),
+            ),
           ),
           Visibility(
               visible: canAddChild,
@@ -193,6 +205,7 @@ class OvcHouseHoldCardBottonContent extends StatelessWidget {
           Visibility(
               visible: canAddChild,
               child: Container(
+                margin: EdgeInsets.symmetric(vertical: 5.0),
                 child: InkWell(
                     onTap: () => onAddNewChild(),
                     child: Container(
---
+++ lib/modules/ovc_intervention/submodules/ovc_services/sub_pages/ovc_child_service_home.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/app_state/intervention_card_state/intervention_card_state.dart';
+import 'package:kb_mobile_app/core/components/Intervention_bottom_navigation_bar_container.dart';
+import 'package:kb_mobile_app/core/components/sub_page_app_bar.dart';
+import 'package:kb_mobile_app/core/components/sup_page_body.dart';
+import 'package:kb_mobile_app/models/intervention_card.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_enrollment_form_save_button.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/sub_pages/asessment/ovc_asessment.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/sub_pages/case%20plan/ovc_case_plan.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/sub_pages/components/ovc_service_child_card.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/sub_pages/constants/ovc_child_service_home_constant.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/sub_pages/monitor/ovc_monitor.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/sub_pages/service/ovc_service_child.dart';
+import 'package:provider/provider.dart';
+
+class OvcChildServiceHome extends StatelessWidget {
+  final String label = 'Child';
+  final List<OvcChildServiceHomeContant> ovcChildServiceHomeCards =
+      OvcChildServiceHomeContant.getOvcChildServiceHomeContant();
+
+  void onOpenChildAssessment(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OvcAssessmentServiceChildView(),
+        ));
+  }
+
+  void onOpenChildCasePlan(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OvcCasePlanChildView(),
+        ));
+  }
+
+  void onOpenChildMonitor(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OvcMonitorChildView(),
+        ));
+  }
+
+  void onOpenChildService(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => OvcServiceSubPageChildView(),
+        ));
+  }
+
+  void childHouseHold() {
+    print("go to house child hold");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(65.0),
+          child: Consumer<IntervetionCardState>(
+            builder: (context, intervetionCardState, child) {
+              InterventionCard activeInterventionProgram =
+                  intervetionCardState.currentIntervetionProgram;
+              return SubPageAppBar(
+                label: label,
+                activeInterventionProgram: activeInterventionProgram,
+              );
+            },
+          ),
+        ),
+        body: SubPageBody(
+            body: Container(
+          child: Column(
+            children: [
+              Container(
+                child: Text('Child info header'),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 16.0, horizontal: 13.0),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  primary: false,
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 10.0,
+                  shrinkWrap: true,
+                  children: ovcChildServiceHomeCards
+                      .map((OvcChildServiceHomeContant
+                              ovcChildServiceHomeCard) =>
+                          Container(
+                            alignment: Alignment.center,
+                            margin: EdgeInsets.all(5.0),
+                            child: InkWell(
+                              child: OvcServiceChildCard(
+                                ovcChildServiceHomeCard:
+                                    ovcChildServiceHomeCard,
+                                countValue: '20',
+                              ),
+                              onTap: () => ovcChildServiceHomeCard.id ==
+                                      'assessment'
+                                  ? onOpenChildAssessment(context)
+                                  : ovcChildServiceHomeCard.id == 'casePlan'
+                                      ? onOpenChildCasePlan(context)
+                                      : ovcChildServiceHomeCard.id == 'services'
+                                          ? onOpenChildService(context)
+                                          : ovcChildServiceHomeCard.id ==
+                                                  'monitor'
+                                              ? onOpenChildMonitor(context)
+                                              : null,
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
+              OvcEnrollmentFormSaveButton(
+                label: "GO TO CHILD'S HOUSE HOLD",
+                labelColor: Colors.white,
+                fontSize: 10,
+                buttonColor: Color(0xFF4B9F46),
+                onPressButton: () => childHouseHold(),
+              ),
+            ],
+          ),
+        )),
+        bottomNavigationBar: InterventionBottomNavigationBarContainer());
+  }
+}
