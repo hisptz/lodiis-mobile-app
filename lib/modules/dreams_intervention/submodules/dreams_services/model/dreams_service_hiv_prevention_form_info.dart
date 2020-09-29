fe6eedb9c4fe3c868dfa28daa53537b774da8ce2
Kijacode <kijadanford@gmail.com>
2020-09-29 17:39:11 +0300

    add form

---
+++ lib/modules/dreams_intervention/submodules/dreams_services/model/dreams_service_hiv_prevention_form_info.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/models/form_section.dart';
+import 'package:kb_mobile_app/models/input_field.dart';
+
+class DreamsServiceHivPreventionInfo {
+  static List<FormSection> getFormSections() {
+    return [
+      FormSection(
+          name: 'HIV Prevention Services offered and Activities Completed',
+          color: Color(0xFF1F8ECE),
+          inputFields: [
+            InputField(
+                id: "W79837fEI3C",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                name: "Name of Youth Mentor/Paralegal",
+                valueType: "TEXT"),
+            InputField(
+                id: "Eug4BXDFLym",
+                name: "Type of Intervention",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                valueType: "TEXT"),
+            InputField(
+              id: "InvIBsm9DiQ",
+              name: "District (where sessions/activities are completed)",
+              valueType: "ORGANISATION_UNIT",
+              inputColor: Color(0xFF1F8ECE),
+              labelColor: Color(0xFF05131B),
+            ),
+            InputField(
+                id: "ZbIgQobmdDR",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                name: "Council (where sessions/activities are completed)",
+                valueType: "ORGANISATION_UNIT"),
+            InputField(
+                id: "BJYUYd4JnuB",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                name: "Village (where sessions/activities are completed)",
+                valueType: "TEXT"),
+            InputField(
+                id: "O7sjTjxUmEa",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                name: "Session date",
+                valueType: "DATE"),
+            InputField(
+                id: "vL6NpUA0rIU",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                name: "Number of hours spent in session",
+                valueType: "INTEGER_ZERO_OR_POSITIVE"),
+            InputField(
+                id: "FoLeDcnocv4",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                name: "Number of Female condoms distributed",
+                valueType: "INTEGER_ZERO_OR_POSITIVE"),
+            InputField(
+                id: "JjX25d72ume",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                name: "Number of Male condoms distributed",
+                valueType: "INTEGER_ZERO_OR_POSITIVE"),
+            InputField(
+                id: "qxO13pu8vAk",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                name: "Number of lubricants distributed",
+                valueType: "INTEGER_ZERO_OR_POSITIVE"),
+            InputField(
+                id: "JGgGQZPZH4n",
+                inputColor: Color(0xFF1F8ECE),
+                labelColor: Color(0xFF05131B),
+                name: "IEC Material Distributed",
+                valueType: "INTEGER_ZERO_OR_POSITIVE"),
+          ])
+    ];
+  }
+}
---
+++ lib/modules/dreams_intervention/submodules/dreams_services/model/dreams_service_tb_screening_form_info.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/models/form_section.dart';
+import 'package:kb_mobile_app/models/input_field.dart';
+
+class DreamsServiceTBScreeningInfo {
+  static List<FormSection> getFormSections() {
+    return [
+      FormSection(name: '', color: Colors.transparent, inputFields: [
+        InputField(
+            id: "YlP3TG4JYKR",
+            inputColor: Color(0xFF1F8ECE),
+            labelColor: Color(0xFF05131B),
+            name: "Pregnant",
+            valueType: "BOOLEAN"),
+        InputField(
+            id: "yIZY0baE8JP",
+            name: "Gestational age",
+            inputColor: Color(0xFF1F8ECE),
+            labelColor: Color(0xFF05131B),
+            valueType: "NUMBER"),
+        InputField(
+          id: "uRsImCXXDXe",
+          name: "HIV Status",
+          valueType: "TEXT",
+          inputColor: Color(0xFF1F8ECE),
+          labelColor: Color(0xFF05131B),
+        ),
+        InputField(
+            id: "tMvluCbiiUm",
+            inputColor: Color(0xFF1F8ECE),
+            labelColor: Color(0xFF05131B),
+            name: "Are you coughing",
+            valueType: "BOOLEAN"),
+        InputField(
+            id: "P9hiqrTjAdg",
+            inputColor: Color(0xFF1F8ECE),
+            labelColor: Color(0xFF05131B),
+            name: "Have you lost weight (without trying)",
+            valueType: "BOOLEAN"),
+        InputField(
+            id: "Y8Xzy7bEWsi",
+            inputColor: Color(0xFF1F8ECE),
+            labelColor: Color(0xFF05131B),
+            name: "Do you have drenching or soaking sweats at night",
+            valueType: "BOOLEAN"),
+        InputField(
+            id: "VETgonq6tFr",
+            inputColor: Color(0xFF1F8ECE),
+            labelColor: Color(0xFF05131B),
+            name: "Do you have fever",
+            valueType: "BOOLEAN"),
+      ])
+    ];
+  }
+}
