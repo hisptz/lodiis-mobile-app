0f5fa54b23ae417923ed278477457be5e236093d
Joseph Chingalo <profschingalo@gmail.com>
2020-10-02 12:01:08 +0300

    feat: support for view listing of case plans

--- lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/components/case_plan_home_list_container.dart
+++ lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/components/case_plan_home_list_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kb_mobile_app/app_state/enrollment_service_form_state/service_event_data_state.dart';
+import 'package:kb_mobile_app/core/components/material_card.dart';
 import 'package:kb_mobile_app/core/utils/tracked_entity_instance_util.dart';
 import 'package:kb_mobile_app/models/events.dart';
 import 'package:provider/provider.dart';
@@ -8,14 +10,18 @@ class CasePlanHomeListContainer extends StatelessWidget {
   const CasePlanHomeListContainer({
     Key key,
     @required this.programStageIds,
+    this.onEditCasePlan,
+    this.onViewCasePlan,
   }) : super(key: key);
 
   final List<String> programStageIds;
+  final Function onViewCasePlan;
+  final Function onEditCasePlan;
 
   @override
   Widget build(BuildContext context) {
+    double iconHeight = 20;
     return Container(
-      margin: EdgeInsets.all(20.0),
       child: Consumer<ServiveEventDataState>(
         builder: (context, serviveEventDataState, child) {
           Map<String, List<Events>> eventListByProgramStage =
@@ -26,7 +32,106 @@ class CasePlanHomeListContainer extends StatelessWidget {
           Map groupedEventByDates =
               TrackedEntityInstanceUtil.getGroupedEventByDates(events);
           return Container(
-            child: Text('Listing ${events.length}'),
+            child: Column(
+              children: groupedEventByDates.keys.toList().map((assessmentDate) {
+                int assessmentIndex = 0;
+                return Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 17.0,
+                  ),
+                  child: MaterialCard(
+                    body: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12.0),
+                        bottomLeft: Radius.circular(12.0),
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border(
+                          left: BorderSide(
+                            color: Color(0xFF4B9F46),
+                            width: 9.0,
+                          ),
+                        )),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Container(
+                                  child: Expanded(
+                                    child: RichText(
+                                      text: TextSpan(
+                                        text: '$assessmentDate   ',
+                                        style: TextStyle().copyWith(
+                                          color: Color(0xFF92A791),
+                                          fontSize: 12.0,
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                        children: [
+                                          TextSpan(
+                                            text:
+                                                'Case plan ${assessmentIndex + 1}',
+                                            style: TextStyle().copyWith(
+                                              color: Color(0xFF1A3518),
+                                              fontSize: 14.0,
+                                              fontWeight: FontWeight.w700,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.symmetric(
+                                    horizontal: 5.0,
+                                  ),
+                                  child: InkWell(
+                                      onTap: () => onViewCasePlan(
+                                          groupedEventByDates[assessmentIndex]),
+                                      child: Container(
+                                        height: iconHeight,
+                                        width: iconHeight,
+                                        margin: EdgeInsets.symmetric(
+                                            vertical: 5, horizontal: 5),
+                                        child: SvgPicture.asset(
+                                          'assets/icons/expand_icon.svg',
+                                          color: Color(0xFF4B9F46),
+                                        ),
+                                      )),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.symmetric(
+                                    horizontal: 5.0,
+                                  ),
+                                  child: InkWell(
+                                      onTap: () => onEditCasePlan(
+                                          groupedEventByDates[assessmentIndex]),
+                                      child: Container(
+                                        height: iconHeight,
+                                        width: iconHeight,
+                                        margin: EdgeInsets.symmetric(
+                                            vertical: 5, horizontal: 5),
+                                        child: SvgPicture.asset(
+                                          'assets/icons/edit-icon.svg',
+                                          color: Color(0xFF4B9F46),
+                                        ),
+                                      )),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
           );
         },
       ),
