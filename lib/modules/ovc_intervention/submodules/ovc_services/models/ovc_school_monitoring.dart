048403bc1dfa2b4ca8a6c26be4c7bfbbb9ac9404
bellangowi <isabellangowi@gmail.com>
2020-10-07 18:54:58 +0300

    Added support to child monitor tool(render,add,view and edit)

--- lib/modules/ovc_intervention/submodules/ovc_services/models/ovc_school_monitoring.dart
+++ lib/modules/ovc_intervention/submodules/ovc_services/models/ovc_school_monitoring.dart
@@ -3,7 +3,7 @@ import 'package:kb_mobile_app/models/form_section.dart';
 import 'package:kb_mobile_app/models/input_field.dart';
 import 'package:kb_mobile_app/models/input_field_option.dart';
 
-class OvcServicesMonitorSchoolPerformance {
+class OvcSchoolMonitoring {
   static List<FormSection> getFormSections() {
     return [
       FormSection(
---
+++ lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/child_monitor/pages/constants/ovc_monitor_constant.dart
@@ -0,0 +1,10 @@
+class OvcMonitorConstant {
+  static Map getOvcMonitorProgramStageMap() {
+    Map map = Map();
+    map['v9Vrc5exzam'] = 'Service';
+    map['dVdlJNtNLIQ'] = 'School';
+    return map;
+  }
+
+  
+}
