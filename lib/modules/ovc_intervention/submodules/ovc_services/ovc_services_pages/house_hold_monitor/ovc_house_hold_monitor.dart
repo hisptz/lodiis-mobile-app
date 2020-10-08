3f8e842dd0956e022172788c413919d3ea0ec5ee
bellangowi <isabellangowi@gmail.com>
2020-10-08 09:15:59 +0300

    Added support to add monitor

--- lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/house_hold_monitor/ovc_house_hold_monitor.dart
+++ lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/house_hold_monitor/ovc_house_hold_monitor.dart
@@ -5,11 +5,20 @@ import 'package:kb_mobile_app/core/components/Intervention_bottom_navigation_bar
 import 'package:kb_mobile_app/core/components/sub_page_app_bar.dart';
 import 'package:kb_mobile_app/core/components/sup_page_body.dart';
 import 'package:kb_mobile_app/models/intervention_card.dart';
+import 'package:kb_mobile_app/models/ovc_house_hold.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_enrollment_form_save_button.dart';
 import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_house_hold_top_header.dart';
 import 'package:provider/provider.dart';
 
 class OvcHouseHoldMonitor extends StatelessWidget {
   final String label = 'House Hold Monitor';
+
+ void onAddNewHouseHoldMonitor(
+    BuildContext context,
+    OvcHouseHold houseHold,
+  ) {
+   
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,9 +49,49 @@ class OvcHouseHoldMonitor extends StatelessWidget {
                         currentOvcHouseHold: currentOvcHouseHold,
                       ),
                       Container(
-                        margin: EdgeInsets.symmetric(
-                            vertical: 16.0, horizontal: 13.0),
-                        child: Text('List to add'),
+                        child: Consumer<OvcHouseHoldCurrentSelectionState>(
+                          builder: (context, ovcHouseHoldCurrentSelectionState,
+                              child) {
+                            OvcHouseHold currentOvcHouseHold =
+                                ovcHouseHoldCurrentSelectionState
+                                    .currentOvcHouseHold;
+                                       return Container(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              Container(
+                                                margin: EdgeInsets.only(
+                                                  top: 10.0,
+                                                  right: 13.0,
+                                                  left: 13.0,
+                                                ),
+                                              ),
+                                              Container(
+                                                child: Visibility(
+                                                  child:
+                                                      OvcEnrollmentFormSaveButton(
+                                                          label:
+                                                              "ADD MONITOR",
+                                                          labelColor:
+                                                              Colors.white,
+                                                          fontSize: 10,
+                                                          buttonColor:
+                                                              Color(0xFF4B9F46),
+                                                          onPressButton: () =>
+                                                              onAddNewHouseHoldMonitor(
+                                                                context,
+                                                                currentOvcHouseHold,
+                                                              )),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                            );
+                          },
+                        ),
                       )
                     ],
                   ),
