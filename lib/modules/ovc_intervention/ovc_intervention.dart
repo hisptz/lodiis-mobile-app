11f8ea03349b592300f07e95bb4de7521da16341
Joseph Chingalo <profschingalo@gmail.com>
2020-10-15 15:32:58 +0300

    feat: support for loading progress on home page of intervention

--- lib/modules/ovc_intervention/ovc_intervention.dart
+++ lib/modules/ovc_intervention/ovc_intervention.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:kb_mobile_app/app_state/enrollment_service_form_state/enrollment_form_state.dart';
 import 'package:kb_mobile_app/app_state/intervention_bottom_navigation_state/intervention_bottom_navigation_state.dart';
 import 'package:kb_mobile_app/app_state/intervention_card_state/intervention_card_state.dart';
 import 'package:kb_mobile_app/core/components/Intervention_bottom_navigation_bar_container.dart';
+import 'package:kb_mobile_app/core/components/circular_process_loader.dart';
 import 'package:kb_mobile_app/core/components/intervention_app_bar.dart';
 import 'package:kb_mobile_app/core/components/route_page_not_found.dart';
 import 'package:kb_mobile_app/core/utils/app_bar_util.dart';
@@ -16,13 +19,31 @@ import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_referral/o
 import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/ovc_services_page.dart';
 import 'package:provider/provider.dart';
 
-class OvcIntervention extends StatelessWidget {
+class OvcIntervention extends StatefulWidget {
   const OvcIntervention({Key key}) : super(key: key);
 
+  @override
+  _OvcInterventionState createState() => _OvcInterventionState();
+}
+
+class _OvcInterventionState extends State<OvcIntervention> {
   final bool disableSelectionOfActiveIntervention = true;
+  bool isViewReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 1), () {
+      setState(() {
+        isViewReady = true;
+      });
+    });
+  }
 
   void onOpenMoreMenu(
-      BuildContext context, InterventionCard activeInterventionProgram) async {
+    BuildContext context,
+    InterventionCard activeInterventionProgram,
+  ) async {
     AppBarUtil.onOpenMoreMenu(
       context,
       activeInterventionProgram,
@@ -69,50 +90,66 @@ class OvcIntervention extends StatelessWidget {
             },
           ),
         ),
-        body: Consumer<IntervetionCardState>(
-            builder: (context, intervetionCardState, child) {
-          InterventionCard activeInterventionProgram =
-              intervetionCardState.currentIntervetionProgram;
-
-          return Stack(
-            fit: StackFit.expand,
-            children: [
-              Container(
-                decoration:
-                    BoxDecoration(color: activeInterventionProgram.background),
-              ),
-              Consumer<InterventionBottomNavigationState>(
-                builder: (context, interventionBottomNavigationState, child) {
-                  InterventionBottomNavigation
-                      currentInterventionBottomNavigation =
-                      interventionBottomNavigationState
-                          .getCurrentInterventionBottomNavigation(
-                              activeInterventionProgram);
-                  return Container(
-                    child: currentInterventionBottomNavigation.id == 'services'
-                        ? OvcServicesPage()
-                        : currentInterventionBottomNavigation.id == 'referral'
-                            ? OvcReferralPage()
-                            : currentInterventionBottomNavigation.id ==
-                                    'enrollment'
-                                ? OvcEnrollmentPage()
-                                : currentInterventionBottomNavigation.id ==
-                                        'exit'
-                                    ? OvcExitPage()
-                                    : currentInterventionBottomNavigation.id ==
-                                            'records'
-                                        ? OvcRecordsPage()
-                                        : RoutePageNotFound(
-                                            pageTitle:
-                                                currentInterventionBottomNavigation
-                                                    .id,
-                                          ),
-                  );
-                },
-              ),
-            ],
-          );
-        }),
+        body: Container(
+          child: !isViewReady
+              ? Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: CircularProcessLoader(
+                    color: Colors.blueGrey,
+                  ),
+                )
+              : Container(
+                  child: Consumer<IntervetionCardState>(
+                      builder: (context, intervetionCardState, child) {
+                    InterventionCard activeInterventionProgram =
+                        intervetionCardState.currentIntervetionProgram;
+                    return Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: activeInterventionProgram.background),
+                        ),
+                        Consumer<InterventionBottomNavigationState>(
+                          builder: (context, interventionBottomNavigationState,
+                              child) {
+                            InterventionBottomNavigation
+                                currentInterventionBottomNavigation =
+                                interventionBottomNavigationState
+                                    .getCurrentInterventionBottomNavigation(
+                                        activeInterventionProgram);
+                            return Container(
+                              child: currentInterventionBottomNavigation.id ==
+                                      'services'
+                                  ? OvcServicesPage()
+                                  : currentInterventionBottomNavigation.id ==
+                                          'referral'
+                                      ? OvcReferralPage()
+                                      : currentInterventionBottomNavigation
+                                                  .id ==
+                                              'enrollment'
+                                          ? OvcEnrollmentPage()
+                                          : currentInterventionBottomNavigation
+                                                      .id ==
+                                                  'exit'
+                                              ? OvcExitPage()
+                                              : currentInterventionBottomNavigation
+                                                          .id ==
+                                                      'records'
+                                                  ? OvcRecordsPage()
+                                                  : RoutePageNotFound(
+                                                      pageTitle:
+                                                          currentInterventionBottomNavigation
+                                                              .id,
+                                                    ),
+                            );
+                          },
+                        ),
+                      ],
+                    );
+                  }),
+                ),
+        ),
         bottomNavigationBar: InterventionBottomNavigationBarContainer(),
       ),
     ));
