3e01e221e8364f68097ecae72ff1b4cffea04e22
Kijacode <kijadanford@gmail.com>
2020-10-22 09:22:37 +0300

    update: pull current update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:kb_mobile_app/app.dart';
+import 'package:flutter_stetho/flutter_stetho.dart';
 
 void main() {
+
+ Stetho.initialize();
   runApp(MyApp());
+  
 }
