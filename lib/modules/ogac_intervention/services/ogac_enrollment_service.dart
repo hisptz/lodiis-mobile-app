498f1992d644882acfc08b08932d4df47928952e
Joseph Chingalo <profschingalo@gmail.com>
2020-10-23 17:27:39 +0300

    feat: support for retrieve events from offline

--- lib/modules/ogac_intervention/services/ogac_enrollment_service.dart
+++ lib/modules/ogac_intervention/services/ogac_enrollment_service.dart
@@ -1,9 +1,14 @@
 import 'package:kb_mobile_app/core/offline_db/enrollment_offline/enrollment_offline_provider.dart';
+import 'package:kb_mobile_app/core/offline_db/event_offline/event_offline_provider.dart';
+import 'package:kb_mobile_app/core/offline_db/tracked_entity_instance_offline/tracked_entity_instance_offline_provider.dart';
+import 'package:kb_mobile_app/core/services/organisation_unit_service.dart';
 import 'package:kb_mobile_app/core/utils/form_util.dart';
 import 'package:kb_mobile_app/core/utils/tracked_entity_instance_util.dart';
 import 'package:kb_mobile_app/models/enrollment.dart';
+import 'package:kb_mobile_app/models/events.dart';
 import 'package:kb_mobile_app/models/form_section.dart';
 import 'package:kb_mobile_app/models/ogac_beneficiary.dart';
+import 'package:kb_mobile_app/models/organisation_unit.dart';
 import 'package:kb_mobile_app/models/tracked_entity_instance.dart';
 import 'package:kb_mobile_app/modules/ogac_intervention/constants/ogac_intervention_constant.dart';
 import 'package:kb_mobile_app/modules/ogac_intervention/models/ogac_enrollment_form_section.dart';
@@ -55,7 +60,30 @@ class OgacEnrollementservice {
     List<OgacBeneficiary> ogacBeneficiaries = [];
     List<Enrollment> enrollments = await EnrollmentOfflineProvider()
         .getEnrollements(OgacInterventionConstant.program);
-    print(enrollments);
+    for (Enrollment enrollment in enrollments) {
+      List<OrganisationUnit> ous = await OrganisationUnitService()
+          .getOrganisationUnits([enrollment.orgUnit]);
+      String location = ous.length > 0 ? ous[0].name : enrollment.orgUnit;
+      String orgUnit = enrollment.orgUnit;
+      String createdDate = enrollment.enrollmentDate;
+      String enrollmentId = enrollment.enrollment;
+      List<TrackeEntityInstance> ogacBeneficiaryList =
+          await TrackedEntityInstanceOfflineProvider()
+              .getTrackedEntityInstance([enrollment.trackedEntityInstance]);
+      for (TrackeEntityInstance tei in ogacBeneficiaryList) {
+        List<Events> eventList = await EventOfflineProvider()
+            .getTrackedEntityInstanceEvents([tei.trackedEntityInstance]);
+        Events eventData = eventList.length > 0 ? eventList[0] : null;
+        ogacBeneficiaries.add(OgacBeneficiary().fromTeiModel(
+          tei,
+          orgUnit,
+          location,
+          createdDate,
+          enrollmentId,
+          eventData,
+        ));
+      }
+    }
     return ogacBeneficiaries;
   }
 
