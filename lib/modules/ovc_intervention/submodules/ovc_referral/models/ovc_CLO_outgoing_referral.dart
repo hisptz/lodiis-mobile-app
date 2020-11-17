5250b90b88b2bdc4e2b475138a07daf3bce34401
bellangowi <isabellangowi@gmail.com>
2020-11-17 09:03:41 +0300

    Mapped CLO Received Referral forms

--- lib/modules/ovc_intervention/submodules/ovc_referral/models/ovc_CLO_outgoing_referral.dart
+++ lib/modules/ovc_intervention/submodules/ovc_referral/models/ovc_CLO_outgoing_referral.dart
@@ -4,7 +4,7 @@ import 'package:kb_mobile_app/models/form_section.dart';
 import 'package:kb_mobile_app/models/input_field.dart';
 import 'package:kb_mobile_app/models/input_field_option.dart';
 
-class OvcCLOutgoingOReferral {
+class OvcCLOOutgoingReferral {
   static List<FormSection> getFormSections() {
     return [
       FormSection(
