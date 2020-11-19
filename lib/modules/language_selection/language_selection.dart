fa4195ba3c94171dc7f33627f3d0e0e95178e467
Joseph Chingalo <profschingalo@gmail.com>
2020-11-19 14:26:30 +0300

    feat: support for module for language translations

--- lib/modules/language_selection/language_selection.dart
+++ lib/modules/language_selection/language_selection.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/core/components/language_selection_container.dart';
+import 'package:kb_mobile_app/core/services/language_selection_service.dart';
+import 'package:kb_mobile_app/modules/login/login.dart';
 
-class LanguageSelection extends StatelessWidget {
+class LanguageSelection extends StatefulWidget {
   const LanguageSelection({Key key}) : super(key: key);
+
+  @override
+  _LanguageSelectionState createState() => _LanguageSelectionState();
+}
+
+class _LanguageSelectionState extends State<LanguageSelection> {
+  void onSetSelectedLanguage(
+    BuildContext context,
+    String selectionLanguageCode,
+  ) async {
+    await LanguageSelectionService.setCurrentLanguageSelection(
+      selectionLanguageCode,
+    );
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Login(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: SafeArea(
-        child: Text('Hello'),
+        child: Scaffold(
+          body: LanguageSelectionContainer(
+              selectionLanguageCode: '',
+              onSetSelectedLanguage: (String selectionLanguageCode) =>
+                  onSetSelectedLanguage(
+                    context,
+                    selectionLanguageCode,
+                  )),
+        ),
       ),
     );
   }
