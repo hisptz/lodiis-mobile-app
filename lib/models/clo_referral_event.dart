0e1238464001b7ad0c045cbb386c05975a88e064
Kijacode <kijadanford@gmail.com>
2020-11-19 11:27:03 +0300

    feat: add clo referral model,expose model from dataObject

---
+++ lib/models/clo_referral_event.dart
@@ -0,0 +1,51 @@
+import 'package:kb_mobile_app/models/events.dart';
+
+class CLOReferralEvent {
+  String id;
+  String referralDate;
+  String referralFor;
+  String outComeStatus;
+  String communityPartner;
+
+  CLOReferralEvent(
+      {this.id,
+      this.referralDate,
+      this.referralFor,
+      this.outComeStatus,
+      this.communityPartner});
+
+  CLOReferralEvent fromTeiModel(
+    Events eventData,
+  ) {
+    List keys = [
+      'OIUDljKyNgy',
+      'rWIw1JHPiTo',
+      'pdUklLmaauR',
+      'RiNop7mvTRW',
+    ];
+    Map data = Map();
+    for (Map detailObj in eventData.dataValues) {
+      String attribute = detailObj['dataElement'];
+      if (attribute != null && keys.indexOf(attribute) > -1) {
+        data[attribute] = '${detailObj['value']}'.trim() ?? '';
+      }
+    }
+
+    return CLOReferralEvent(
+      id: eventData.event,
+      referralDate: data['OIUDljKyNgy'] ?? eventData.eventDate,
+      referralFor: data['rWIw1JHPiTo'] ?? '',
+      outComeStatus: data['pdUklLmaauR'] != null && data['pdUklLmaauR'] != ''
+          ? data['pdUklLmaauR']
+          : '',
+      communityPartner: data['RiNop7mvTRW'] != null && data['RiNop7mvTRW'] != ''
+          ? data['RiNop7mvTRW']
+          : '',
+    );
+  }
+
+  @override
+  String toString() {
+    return '$id $referralDate';
+  }
+}
