eb274698c337774a4fa0549ce363e7b4665ec2d0
chingaloEric <echingalo@gmail.com>
2020-11-24 10:52:57 +0300

    refactor(ovc_exit): added more skip logics

---
+++ lib/modules/ovc_intervention/submodules/ovc_exit/ovc_exit_pages/child_exit_pages/skip_logics/ovc_child_case_plan_graduation_skip_logic.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/app_state/enrollment_service_form_state/service_form_state.dart';
+import 'package:kb_mobile_app/core/utils/form_util.dart';
+import 'package:kb_mobile_app/models/form_section.dart';
+import 'package:provider/provider.dart';
+
+class OvcChildCasePlanGraduationSkipLogic {
+  static Map hiddenFields = Map();
+  static Map hiddenSections = Map();
+
+  static Future evaluateSkipLogics(
+    BuildContext context,
+    List<FormSection> formSections,
+    Map dataObject,
+  ) async {
+    hiddenFields.clear();
+    hiddenSections.clear();
+    List<String> inputFieldIds = FormUtil.getFormFieldIds(formSections);
+    for (var key in dataObject.keys) {
+      inputFieldIds.add('$key');
+    }
+    inputFieldIds = inputFieldIds.toSet().toList();
+    for (String inputFieldId in inputFieldIds) {
+      String value = '${dataObject[inputFieldId]}';
+      if (inputFieldId == 'RK3fD7HvH9Q' && value != 'true') {
+        hiddenFields['wGeVdyVO5hE'] = true;
+      }
+      if(inputFieldId == 'l9HIQkANCHj' && value != 'true') {
+        hiddenFields['l9HIQkANCHj_checkbox'] = true;
+      }
+      if (inputFieldId == 'fffETOzhGsU' && value != 'true') {
+        hiddenFields['fffETOzhGsU_checkbox'] = true;
+        hiddenFields['gSgJKdbwTSs'] = true;
+        hiddenFields['bhXYz7AnnN7'] = true;
+        hiddenFields['dRiKVfftiaX'] = true;
+      }
+    }
+    for (String sectionId in hiddenSections.keys) {
+      List<String> inputFieldIds = FormUtil.getFormFieldIds(formSections
+          .where((formSection) => formSection.id == sectionId)
+          .toList());
+      for (String inputFieldId in inputFieldIds) {
+        hiddenFields[inputFieldId] = true;
+      }
+    }
+    resetValuesForHiddenFields(context, hiddenFields.keys);
+    resetValuesForHiddenSections(context, formSections);
+  }
+
+  static resetValuesForHiddenFields(BuildContext context, inputFieldIds) {
+    for (String inputFieldId in inputFieldIds) {
+      if (hiddenFields[inputFieldId]) {
+        assignInputFieldValue(context, inputFieldId, null);
+      }
+    }
+    Provider.of<ServiceFormState>(context, listen: false)
+        .setHiddenFields(hiddenFields);
+  }
+
+  static resetValuesForHiddenSections(
+    BuildContext context,
+    List<FormSection> formSections,
+  ) {
+    Provider.of<ServiceFormState>(context, listen: false)
+        .setHiddenSections(hiddenSections);
+  }
+
+  static assignInputFieldValue(
+    BuildContext context,
+    String inputFieldId,
+    String value,
+  ) {
+    Provider.of<ServiceFormState>(context, listen: false)
+        .setFormFieldState(inputFieldId, value);
+  }
+}
