d812c0524f3c702e19f14ed1d1f95cacf08ce085
chingaloEric <echingalo@gmail.com>
2020-11-25 19:04:11 +0300

    refactor(dreams_enrollment): restricted enrollment date to between 9 to 24

--- lib/modules/dreams_intervention/submodules/dreams_enrollment/models/agyw_enrollment_risk_assessment.dart
+++ lib/modules/dreams_intervention/submodules/dreams_enrollment/models/agyw_enrollment_risk_assessment.dart
@@ -72,7 +72,10 @@ class AgywEnrollmentRiskAssessment {
                 name: 'Date of Birth',
                 valueType: 'DATE',
                 inputColor: Color(0xFF258DCC),
-                labelColor: Color(0xFF737373)),
+                labelColor: Color(0xFF737373),
+                maxAgeInYear: 24,
+                minAgeInYear: 9,
+                hint: "Beneficiary's age should be from 9 - 24 years",),
             InputField(
                 id: 'ls9hlz2tyol',
                 name: 'Age',
@@ -715,7 +718,69 @@ class AgywEnrollmentRiskAssessment {
                             inputColor: Color(0xFF258DCC),
                             labelColor: Color(0xFF737373)),
                     ]),
-          ])
+          ]),
+          FormSection(
+          name: 'Enrollment Criteria',
+          color: Color(0xFF258DCC),
+          inputFields: [
+            InputField(
+                id: 'B4ojlzKypUF',
+                name: 'Is Orphanhood (AGYW 10-17 Yrs)',
+                valueType: 'TRUE_ONLY',
+                isReadOnly: true,
+                inputColor: Color(0xFF258DCC),
+                labelColor: Color(0xFF737373)),
+            InputField(
+                id: 'HJIHPjOf5s1',
+                name: 'Alcohol Use',
+                valueType: 'TRUE_ONLY',
+                inputColor: Color(0xFF258DCC),
+                isReadOnly: true,
+                labelColor: Color(0xFF737373)),
+            InputField(
+                id: 'bApA6X6TVvJ',
+                name: 'Out of school/Never schooled education/Enrollment',
+                valueType: 'TRUE_ONLY',
+                inputColor: Color(0xFF258DCC),
+                isReadOnly: true,
+                labelColor: Color(0xFF737373)),
+            InputField(
+                id: 'yc3BlUIEup3',
+                name: 'Experience of violence',
+                valueType: 'TRUE_ONLY',
+                inputColor: Color(0xFF258DCC),
+                isReadOnly: true,
+                labelColor: Color(0xFF737373)),
+            InputField(
+                id: 'p33MrrhsQRz',
+                name: 'Transactional Sex',
+                valueType: 'TRUE_ONLY',
+                inputColor: Color(0xFF258DCC),
+                isReadOnly: true,
+                labelColor: Color(0xFF737373)),
+            InputField(
+                id: 'iY67qYnywgS',
+                name: 'No or Irregular Condom use',
+                valueType: 'TRUE_ONLY',
+                inputColor: Color(0xFF258DCC),
+                isReadOnly: true,
+                labelColor: Color(0xFF737373)),
+            InputField(
+                id: 'rh881j2vfvT',
+                name:
+                    'Sexually Transmitted Infection (STI) - (past or present)',
+                valueType: 'TRUE_ONLY',
+                inputColor: Color(0xFF258DCC),
+                isReadOnly: true,
+                labelColor: Color(0xFF737373)),
+            InputField(
+                id: 'AZCVLPzD0Vd',
+                name: 'High number of sex partners',
+                valueType: 'TRUE_ONLY',
+                inputColor: Color(0xFF258DCC),
+                isReadOnly: true,
+                labelColor: Color(0xFF737373)),
+          ]),
     ];
   }
 }
