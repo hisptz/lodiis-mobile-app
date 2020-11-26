ba225c7e2251de761205fe5be69b027d776ca212
Joseph Chingalo <profschingalo@gmail.com>
2020-11-26 13:03:12 +0300

    chore: try and catch on saving online data

--- lib/app_state/synchronization_state/synchronization_state.dart
+++ lib/app_state/synchronization_state/synchronization_state.dart
@@ -48,7 +48,6 @@ class SynchronizationState with ChangeNotifier {
 
   List<Events> get eventFromServer => _eventFromServer ?? [];
 
-
   int get conflictCount => _conflictLCount ?? 0;
 
   List<TrackeEntityInstance> get trackedEntityInstanceFromServer =>
@@ -158,14 +157,18 @@ class SynchronizationState with ChangeNotifier {
   Future saveTrackedEntityStateToOffline() async {
     addDataDownloadProcess('Saving Profiles');
     for (var instance in servertrackedEntityInstance) {
-      await _synchronizationService
-          .saveEnrollmentToOffline(instance['enrollments']);
-      await _synchronizationService.saveRelationshipsToOffline(instance['relationships']);
-      await _synchronizationService.saveTrackeEntityInstanceToOffline(
-          TrackeEntityInstance().fromJson(instance));
+      try {
+        await _synchronizationService
+            .saveEnrollmentToOffline(instance['enrollments']);
+        await _synchronizationService
+            .saveRelationshipsToOffline(instance['relationships']);
+        await _synchronizationService.saveTrackeEntityInstanceToOffline(
+            TrackeEntityInstance().fromJson(instance));
+      } catch (e) {
+        print(e.toString());
+      }
     }
   }
-
 
   Future analysisOfDownloadedData() async {
     addDataDownloadProcess("Start analyse service data ");
