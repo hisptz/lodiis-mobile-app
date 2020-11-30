cef276222ed3aa6f828cf6bce80ba46cc591d712
chingaloEric <echingalo@gmail.com>
2020-11-30 10:50:28 +0300

    refactor(agyw_dreams): removed the unneeded forms

--- lib/modules/dreams_intervention/submodules/dreams_services/sub_modules/service/constants/service_form_constant.dart
+++
@@ -1,4 +0,0 @@
-class ServiceFormConstant {
-  static final String program = 'hOEIHJDrrvz';
-  static final String programStage = 'bDJq2JWVTbC';
-}
--- lib/modules/dreams_intervention/submodules/dreams_services/sub_modules/stepping_stones/constants/stepping_steps_constant.dart
+++
@@ -1,4 +0,0 @@
-class SteppingStepsConstant  {
-  static final String program = 'hOEIHJDrrvz';
-  static final String programStage = 'i9sW7OP4xZs';
-}
