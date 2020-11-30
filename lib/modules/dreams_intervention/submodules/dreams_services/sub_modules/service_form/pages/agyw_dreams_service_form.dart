94a5e50b4278e5ba1486d7e0276462ffc5bcf326
chingaloEric <echingalo@gmail.com>
2020-11-30 10:51:31 +0300

    feat(agyw_dreams): added a route to services form
    this form is replace the previous deleted forms

---
+++ lib/modules/dreams_intervention/submodules/dreams_services/sub_modules/service_form/pages/agyw_dreams_service_form.dart
@@ -0,0 +1,197 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:kb_mobile_app/app_state/dreams_intervention_list_state/dream_current_selection_state.dart';
+import 'package:kb_mobile_app/app_state/enrollment_service_form_state/service_event_data_state.dart';
+import 'package:kb_mobile_app/app_state/enrollment_service_form_state/service_form_state.dart';
+import 'package:kb_mobile_app/app_state/intervention_card_state/intervention_card_state.dart';
+import 'package:kb_mobile_app/core/components/Intervention_bottom_navigation_bar_container.dart';
+import 'package:kb_mobile_app/core/components/circular_process_loader.dart';
+import 'package:kb_mobile_app/core/components/entry_forms/entry_form_container.dart';
+import 'package:kb_mobile_app/core/components/sub_page_app_bar.dart';
+import 'package:kb_mobile_app/core/components/sup_page_body.dart';
+import 'package:kb_mobile_app/core/utils/app_util.dart';
+import 'package:kb_mobile_app/core/utils/tracked_entity_instance_util.dart';
+import 'package:kb_mobile_app/models/agyw_dream.dart';
+import 'package:kb_mobile_app/models/form_section.dart';
+import 'package:kb_mobile_app/models/intervention_card.dart';
+import 'package:kb_mobile_app/modules/dreams_intervention/components/dream_beneficiary_top_header.dart';
+import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_services/models/dreams_service_service_form.dart';
+import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_services/sub_modules/service_form/constants/service_form_constant.dart';
+import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_services/sub_modules/service_form/skip_logics/agyw_dreams_service_form_skip_logic.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_enrollment_form_save_button.dart';
+import 'package:provider/provider.dart';
+
+class AgywDreamsServiceForm extends StatefulWidget {
+  AgywDreamsServiceForm({Key key}) : super(key: key);
+
+  @override
+  _AgywDreamsServiceFormState createState() => _AgywDreamsServiceFormState();
+}
+
+class _AgywDreamsServiceFormState extends State<AgywDreamsServiceForm> {
+  final String label = 'Service Form';
+  List<FormSection> formSections;
+  bool isFormReady = false;
+  bool isSaving = false;
+
+  @override
+  void initState() {
+    super.initState();
+    formSections = DreamsServiceForm.getFormSections();
+    Timer(Duration(seconds: 1), () {
+      setState(() {
+        isFormReady = true;
+        evaluateSkipLogics();
+      });
+    });
+  }
+
+  evaluateSkipLogics() {
+    Timer(
+      Duration(milliseconds: 200),
+      () async {
+        Map dataObject =
+            Provider.of<ServiceFormState>(context, listen: false).formState;
+        await AgywDreamsServiceFormSkipLogic.evaluateSkipLogics(
+          context,
+          formSections,
+          dataObject,
+        );
+      },
+    );
+  }
+
+  void onInputValueChange(String id, dynamic value) {
+    Provider.of<ServiceFormState>(context, listen: false)
+        .setFormFieldState(id, value);
+    evaluateSkipLogics();
+  }
+
+  void onSaveForm(
+      BuildContext context, Map dataObject, AgywDream agywDream) async {
+    if (dataObject.keys.length > 0) {
+      setState(() {
+        isSaving = true;
+      });
+      String eventDate = dataObject['eventDate'];
+      String eventId = dataObject['eventId'];
+      List<String> hiddenFields = [];
+      try {
+        await TrackedEntityInstanceUtil.savingTrackedEntityInstanceEventData(
+            ServiceFormConstant.program,
+            ServiceFormConstant.programStage,
+            agywDream.orgUnit,
+            formSections,
+            dataObject,
+            eventDate,
+            agywDream.id,
+            eventId,
+            hiddenFields);
+        Provider.of<ServiveEventDataState>(context, listen: false)
+            .resetServiceEventDataState(agywDream.id);
+        Timer(Duration(seconds: 1), () {
+          setState(() {
+            AppUtil.showToastMessage(
+                message: 'Form has been saved successfully',
+                position: ToastGravity.TOP);
+            Navigator.pop(context);
+          });
+        });
+      } catch (e) {
+        Timer(Duration(seconds: 1), () {
+          setState(() {
+            AppUtil.showToastMessage(
+                message: e.toString(), position: ToastGravity.BOTTOM);
+          });
+        });
+      }
+    } else {
+      AppUtil.showToastMessage(
+          message: 'Please fill at least one form field',
+          position: ToastGravity.TOP);
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(65.0),
+          child: Consumer<IntervetionCardState>(
+            builder: (context, intervetionCardState, child) {
+              InterventionCard activeInterventionProgram =
+                  intervetionCardState.currentIntervetionProgram;
+              return SubPageAppBar(
+                label: label,
+                activeInterventionProgram: activeInterventionProgram,
+              );
+            },
+          ),
+        ),
+        body: SubPageBody(
+          body: Container(child: Consumer<DreamBenefeciarySelectionState>(
+            builder: (context, nonAgywState, child) {
+              AgywDream agywDream = nonAgywState.currentAgywDream;
+              return Consumer<ServiceFormState>(
+                builder: (context, serviceFormState, child) {
+                  return Container(
+                    child: Column(
+                      children: [
+                        DreamBenefeciaryTopHeader(
+                          agywDream: agywDream,
+                        ),
+                        !isFormReady
+                            ? Container(
+                                child: CircularProcessLoader(
+                                  color: Colors.blueGrey,
+                                ),
+                              )
+                            : Column(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.only(
+                                      top: 10.0,
+                                      left: 13.0,
+                                      right: 13.0,
+                                    ),
+                                    child: EntryFormContainer(
+                                      hiddenFields:
+                                          serviceFormState.hiddenFields,
+                                      hiddenSections:
+                                          serviceFormState.hiddenSections,
+                                      formSections: formSections,
+                                      mandatoryFieldObject: Map(),
+                                      isEditableMode:
+                                          serviceFormState.isEditableMode,
+                                      dataObject: serviceFormState.formState,
+                                      onInputValueChange: onInputValueChange,
+                                    ),
+                                  ),
+                                  Visibility(
+                                    visible: serviceFormState.isEditableMode,
+                                    child: OvcEnrollmentFormSaveButton(
+                                      label: isSaving ? 'Saving ...' : 'Save',
+                                      labelColor: Colors.white,
+                                      buttonColor: Color(0xFF258DCC),
+                                      fontSize: 15.0,
+                                      onPressButton: () => onSaveForm(
+                                          context,
+                                          serviceFormState.formState,
+                                          agywDream),
+                                    ),
+                                  )
+                                ],
+                              )
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+          )),
+        ),
+        bottomNavigationBar: InterventionBottomNavigationBarContainer());
+  }
+}
