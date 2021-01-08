e6091fe34e3213453e3a0cd85b78253bda2e5e83
chingaloEric <echingalo@gmail.com>
2021-01-08 16:10:47 +0300

    fix: 🐛 resolved hidding of fields for referral froms

--- lib/modules/dreams_intervention/submodules/dreams_services/sub_modules/srh/skip_logics/agyw_dreams_srh_register_skip_logic.dart
+++ lib/modules/dreams_intervention/submodules/dreams_services/sub_modules/srh/skip_logics/agyw_dreams_srh_register_skip_logic.dart
@@ -28,15 +28,10 @@ class AgywDreamsSrhRegisterSkipLogic {
           FormUtil.getFlattenFormSections(formSections);
       List<String> hidddenSectionInputFieldIds = FormUtil.getFormFieldIds(allFormSections
           .where((formSection) => formSection.id == sectionId)
-          .toList());
-      List<String> allInputFieldIds = FormUtil.getFormFieldIds(allFormSections
-          .where((formSection) => formSection.id != sectionId)
-          .toList());
-      
+          .toList());      
       for (String inputFieldId in hidddenSectionInputFieldIds) {
-        if (allInputFieldIds.indexOf(inputFieldId) == -1) {
-          hiddenFields[inputFieldId] = true;
-        }
+
+        hiddenFields[inputFieldId] = true;
       }
     }
     resetValuesForHiddenFields(context, hiddenFields.keys);
--- lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/skip_logics/ovc_case_plan_gap_skip_logic.dart
+++ lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/skip_logics/ovc_case_plan_gap_skip_logic.dart
@@ -265,18 +265,12 @@ mixin OvcCasePlanGapSkipLogic {
     for (String sectionId in hiddenSections.keys) {
       List<FormSection> allFormSections =
           FormUtil.getFlattenFormSections(formSections);
-      List<String> hidddenSectionInputFieldIds = FormUtil.getFormFieldIds(
-          allFormSections
-              .where((formSection) => formSection.id == sectionId)
-              .toList());
-      List<String> allInputFieldIds = FormUtil.getFormFieldIds(allFormSections
-          .where((formSection) => formSection.id != sectionId)
-          .toList());
-
+      List<String> hidddenSectionInputFieldIds = FormUtil.getFormFieldIds(allFormSections
+          .where((formSection) => formSection.id == sectionId)
+          .toList());      
       for (String inputFieldId in hidddenSectionInputFieldIds) {
-        if (allInputFieldIds.indexOf(inputFieldId) == -1) {
-          hiddenFields[inputFieldId] = true;
-        }
+
+        hiddenFields[inputFieldId] = true;
       }
     }
     resetValuesForHiddenFields(context, hiddenFields.keys);
