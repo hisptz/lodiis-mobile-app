0dcc45ba4cc7a4b3731da16c902d0fbf8eed71ff
chingaloEric <echingalo@gmail.com>
2021-03-15 15:01:35 +0300

    fix(input_field): restrited the clearing of input values to only to specific value types

--- lib/core/components/input_fields/input_field_container.dart
+++ lib/core/components/input_fields/input_field_container.dart
@@ -376,7 +376,16 @@ class InputFieldContainer extends StatelessWidget {
                       showClearIcon: inputField != null &&
                           dataObject[inputField.id] != null &&
                           dataObject[inputField.id] != '' &&
-                          inputField.isReadOnly == false,
+                          inputField.isReadOnly == false &&
+                          ([
+                                'LONG_TEXT',
+                                'TEXT',
+                                'INTEGER_ZERO_OR_POSITIVE',
+                                'NUMBER',
+                                'PHONE_NUMBER',
+                                'CHECK_BOX'
+                              ].indexOf(inputField.valueType) ==
+                              -1),
                       onClearInput: () {
                         onInputValueChange(inputField.id, null);
                       })
