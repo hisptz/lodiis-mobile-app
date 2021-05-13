46ce4d41c509cf42bc1c8056fe13c821f24a8681
Joseph Chingalo <profschingalo@gmail.com>
2021-05-13 11:37:45 +0300

    feat: support for transformation to flutter models for referral nofifications

--- lib/models/referralEventNotification.dart
+++ lib/models/referralEventNotification.dart
@@ -12,4 +12,24 @@ class ReferralEventNotification {
   }) {
     this.isCompleted = this.isCompleted ?? false;
   }
+
+  factory ReferralEventNotification.fromJson(Map json) {
+    String id = json["id"] ?? "";
+    String tei = json["tei"] ?? "";
+    bool isCompleted = json["isCompleted"] ?? false;
+    return ReferralEventNotification(
+      id: id,
+      tei: tei,
+      isCompleted: isCompleted,
+    );
+  }
+
+  factory ReferralEventNotification.toJSon() {
+    return null;
+  }
+
+  @override
+  String toString() {
+    return "<id :$id, tei : $tei, isCompleted:$isCompleted>";
+  }
 }
--- lib/models/referralNotification.dart
+++ lib/models/referralNotification.dart
@@ -17,4 +17,30 @@ class ReferralNotification {
   }) {
     this.referrals = this.referrals ?? [];
   }
+
+  factory ReferralNotification.fromJson(Map data) {
+    String id = data["id"] ?? "";
+    String implementingPartner = data["implementingPartner"] ?? "";
+    String location = data["location"] ?? "";
+    String tei = data["tei"] ?? "";
+    List referrals = (data["referrals"] as List ?? [])
+        .map((dynamic json) => ReferralEventNotification.fromJson(json))
+        .toList();
+    return ReferralNotification(
+      id: id,
+      implementingPartner: implementingPartner,
+      location: location,
+      tei: tei,
+      referrals: referrals,
+    );
+  }
+
+  factory ReferralNotification.toJSon() {
+    return null;
+  }
+
+  @override
+  String toString() {
+    return "$id $implementingPartner $location $tei";
+  }
 }
