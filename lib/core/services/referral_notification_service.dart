d537eeb3838ed96e51159e549818d3b7108ce721
Joseph Chingalo <profschingalo@gmail.com>
2021-05-13 11:00:36 +0300

    feat: support for getting keys for referral notifications based on login IP

--- lib/core/services/referral_notification_service.dart
+++ lib/core/services/referral_notification_service.dart
@@ -1,3 +1,62 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:kb_mobile_app/core/services/http_service.dart';
+import 'package:kb_mobile_app/core/services/user_service.dart';
+import 'package:kb_mobile_app/models/current_user.dart';
+import 'package:kb_mobile_app/models/referralNotification.dart';
+
 class ReferralNotificationService {
-  final String dataStoreNameSpace = "referral-notification";
+  final String dataStoreNameSpace = "kb-referral-notification";
+
+  discoveringAndSaveReferralNotificationFromServer() async {
+    try {
+      CurrentUser currentUser = await UserService().getCurrentUser();
+      HttpService httpService = HttpService(
+        username: currentUser.username,
+        password: currentUser.password,
+      );
+      String implementingPartner =
+          currentUser.implementingPartner.split("/").join("-");
+      String url = "api/dataStore/$dataStoreNameSpace";
+      Response response = await httpService.httpGet(url, queryParameters: {});
+      List<String> keysForReferralNotification = getKeysForReferralNofification(
+        response,
+        implementingPartner,
+      );
+      print(keysForReferralNotification);
+    } catch (error) {
+      print("errror : $error");
+    }
+  }
+
+  Future<List<ReferralNotification>> getReferralNotificationFromServer(
+    List<String> keysForReferralNotification,
+    HttpService httpService,
+  ) async {
+    List<ReferralNotification> referralNofications = [];
+    try {
+      Response response = null;
+    } catch (error) {
+      print("error : $error");
+    }
+    return referralNofications;
+  }
+
+  List<String> getKeysForReferralNofification(
+    Response response,
+    String implementingPartner,
+  ) {
+    List<String> selectedKeys = [];
+    try {
+      for (String key in json.decode(response.body)) {
+        if (key.contains(implementingPartner)) {
+          selectedKeys.add(key);
+        }
+      }
+    } catch (error) {
+      print("error : $error");
+    }
+    return selectedKeys;
+  }
 }
