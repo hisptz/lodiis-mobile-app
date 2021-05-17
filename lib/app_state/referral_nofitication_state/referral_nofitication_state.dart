4cfff54a7d2ad99cec3cf2436e5a76dcc2168d4a
Joseph Chingalo <profschingalo@gmail.com>
2021-05-17 11:11:24 +0300

    feat: support for set and getting referral notifications

--- lib/app_state/referral_nofitication_state/referral_nofitication_state.dart
+++ lib/app_state/referral_nofitication_state/referral_nofitication_state.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/foundation.dart';
 import 'package:kb_mobile_app/core/services/referral_notification_service.dart';
+import 'package:kb_mobile_app/models/referralEventNotification.dart';
+import 'package:kb_mobile_app/models/referralNotification.dart';
 
 class ReferralNotificationState with ChangeNotifier {
   // initial state
+  String _currentImplementingPartner;
+  List<String> _beneficiariesWithIncomingReferrals;
+  List<String> _incommingReferrals;
 
   // Selectors
+  String get currentImplementingPartner => _currentImplementingPartner ?? "";
+  List<String> get beneficiariesWithIncomingReferrals =>
+      _beneficiariesWithIncomingReferrals ?? [];
+  List<String> get incomingReferrals => _incommingReferrals ?? [];
 
   // reducer for the state
+  setCurrentImplementingPartner(String implementingPartner) {
+    print("implementingPartner $implementingPartner");
+    _currentImplementingPartner = implementingPartner;
+    notifyListeners();
+    print("implementingPartner $implementingPartner");
+  }
+
+  reloadReferralNotifications() async {
+    _beneficiariesWithIncomingReferrals = [];
+    _incommingReferrals = [];
+    List<ReferralNotification> referralNofications =
+        await ReferralNotificationService()
+            .getReferralNotificationFromOffline();
+    for (ReferralNotification referralNotification in referralNofications) {
+      List<ReferralEventNotification> referrals = referralNotification.referrals
+          .where((ReferralEventNotification referral) => !referral.isCompleted)
+          .toList();
+      if (referrals.length > 0) {
+        _beneficiariesWithIncomingReferrals.add(referralNotification.tei);
+        _incommingReferrals.addAll(referrals
+            .map((ReferralEventNotification referral) => referral.id ?? "")
+            .toList());
+      }
+    }
+    notifyListeners();
+  }
 
   updateReferralNotificaionEvent(String referralEventId, String tei) async {
     await ReferralNotificationService()
         .updateReferralNotificaionEvent(referralEventId, tei);
-    notifyListeners();
   }
 }
