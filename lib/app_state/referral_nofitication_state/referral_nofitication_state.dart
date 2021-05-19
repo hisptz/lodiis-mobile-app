23cc18ef774ec450b6bf3285fb740346242d46aa
Joseph Chingalo <profschingalo@gmail.com>
2021-05-19 10:03:04 +0300

    feat: support for referral notification indicator

--- lib/app_state/referral_nofitication_state/referral_nofitication_state.dart
+++ lib/app_state/referral_nofitication_state/referral_nofitication_state.dart
@@ -8,12 +8,16 @@ class ReferralNotificationState with ChangeNotifier {
   String _currentImplementingPartner;
   List<String> _beneficiariesWithIncomingReferrals;
   List<String> _incommingReferrals;
+  String _incomingReferralToResolve;
+  String _incomingReferralsResolved;
 
   // Selectors
   String get currentImplementingPartner => _currentImplementingPartner ?? "";
   List<String> get beneficiariesWithIncomingReferrals =>
       _beneficiariesWithIncomingReferrals ?? [];
   List<String> get incomingReferrals => _incommingReferrals ?? [];
+  String get incomingReferralsResolved => _incomingReferralsResolved ?? "";
+  String get incomingReferralToResolve => _incomingReferralToResolve ?? "";
 
   // reducer for the state
   Future<void> setCurrentImplementingPartner(String implementingPartner) async {
@@ -25,27 +29,48 @@ class ReferralNotificationState with ChangeNotifier {
   Future<void> reloadReferralNotifications() async {
     _beneficiariesWithIncomingReferrals = [];
     _incommingReferrals = [];
-    List<ReferralNotification> referralNofications =
-        (await ReferralNotificationService()
-                .getReferralNotificationFromOffline())
-            .where((ReferralNotification referralNotification) =>
-                referralNotification.implementingPartner ==
-                _currentImplementingPartner)
+    int incomingReferralToResolveCount = 0;
+    int incomingReferralsResolvedCount = 0;
+    List<ReferralNotification> allReferralNofications =
+        await ReferralNotificationService()
+            .getReferralNotificationFromOffline();
+    for (ReferralNotification referralNotification in allReferralNofications) {
+      if (referralNotification.implementingPartner ==
+          _currentImplementingPartner) {
+        List<ReferralEventNotification> referrals = referralNotification
+            .referrals
+            .where(
+                (ReferralEventNotification referral) => !referral.isCompleted)
             .toList();
-    for (ReferralNotification referralNotification in referralNofications) {
-      List<ReferralEventNotification> referrals = referralNotification.referrals
-          .where((ReferralEventNotification referral) => !referral.isCompleted)
-          .toList();
-      if (referrals.length > 0) {
-        _beneficiariesWithIncomingReferrals.add(referralNotification.tei);
-        _incommingReferrals.addAll(referrals
-            .map((ReferralEventNotification referral) => referral.id ?? "")
-            .toList());
+        incomingReferralToResolveCount += referrals.length;
+        if (referrals.length > 0) {
+          _beneficiariesWithIncomingReferrals.add(referralNotification.tei);
+          _incommingReferrals.addAll(referrals
+              .map((ReferralEventNotification referral) => referral.id ?? "")
+              .toList());
+        }
+      } else {
+        List<ReferralEventNotification> referrals = referralNotification
+            .referrals
+            .where((ReferralEventNotification referral) =>
+                referral.isCompleted && referral.isViewed)
+            .toList();
+        incomingReferralsResolvedCount += referrals.length;
       }
     }
     _beneficiariesWithIncomingReferrals =
         _beneficiariesWithIncomingReferrals.toSet().toList();
     _incommingReferrals = _incommingReferrals.toSet().toList();
+    _incomingReferralsResolved = incomingReferralsResolvedCount == 0
+        ? ""
+        : incomingReferralsResolvedCount > 9
+            ? "9+"
+            : "$incomingReferralsResolvedCount";
+    _incomingReferralToResolve = incomingReferralToResolveCount == 0
+        ? ""
+        : incomingReferralToResolveCount > 9
+            ? "9+"
+            : "$incomingReferralToResolveCount";
     notifyListeners();
   }
 
