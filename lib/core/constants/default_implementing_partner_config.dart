65f95f0f3add4fb04df530e3d4284a477f70ecec
chingaloEric <echingalo@gmail.com>
2021-05-19 17:07:19 +0300

    refactor(sync): donwloaded only beneficiaries and events for programs related to the IP

---
+++ lib/core/constants/default_implementing_partner_config.dart
@@ -0,0 +1,5 @@
+class DefaultImplementingPartnerConfig {
+  static String getDefaultConfig() {
+    return '{"CLO":["hOEIHJDrrvz","M4uWNFJNMIR","BNsDaCclOiu","em38qztTI8s","AxscuxwznKK"],"PSI":["hOEIHJDrrvz","M4uWNFJNMIR"],"EGPAF":["hOEIHJDrrvz","M4uWNFJNMIR"],"JHPIEGO":["hOEIHJDrrvz","M4uWNFJNMIR"],"Paralegal":["hOEIHJDrrvz","M4uWNFJNMIR","BNsDaCclOiu","em38qztTI8s","AxscuxwznKK"],"Super user":["hOEIHJDrrvz","M4uWNFJNMIR","BNsDaCclOiu","em38qztTI8s","AxscuxwznKK","dwtPhZrg2k7","CK4iMK8b0aZ"],"KB-AGYW/DREAMS":["hOEIHJDrrvz","M4uWNFJNMIR","BNsDaCclOiu","em38qztTI8s","AxscuxwznKK","dwtPhZrg2k7","CK4iMK8b0aZ"],"KB-Case Management":["dwtPhZrg2k7","BNsDaCclOiu","em38qztTI8s"]}';
+  }
+}
---
+++ lib/core/services/implementing_partner_config_service.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:kb_mobile_app/core/constants/default_implementing_partner_config.dart';
+import 'package:kb_mobile_app/core/services/http_service.dart';
+
+class ImplementingPartnerConfigService {
+  final String url = "api/dataStore/kb-mobile-app/implementing-partner-program";
+
+  Future<dynamic> getImplementingPartnerConfigFromTheServer(
+    String username,
+    String password,
+  ) async {
+    String defaultImplementingPartnerConfig =
+        DefaultImplementingPartnerConfig.getDefaultConfig();
+    String currentImplementingPartnerConfig = '';
+
+    try {
+      HttpService http = new HttpService(
+        username: username,
+        password: password,
+      );
+      var response = await http.httpGet(url);
+      currentImplementingPartnerConfig = response.statusCode == 200
+          ? response.body
+          : defaultImplementingPartnerConfig;
+    } catch (error) {
+      print('getImplementingPartnerConfigFromTheServer: ${error.toString()}');
+    }
+    return json.decode(currentImplementingPartnerConfig);
+  }
+}
