8b711c5657d913299b26dfbe538385300553458e
chingaloEric <echingalo@gmail.com>
2021-05-31 12:35:54 +0300

    refactor(search_input): made the search input shared on core components

--- lib/modules/dreams_intervention/submodules/dreams_referral/components/dreams_outgoing_referrals_outcome.dart
+++ lib/modules/dreams_intervention/submodules/dreams_referral/components/dreams_outgoing_referrals_outcome.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:kb_mobile_app/core/components/input_fields/search_input.dart';
 import 'package:kb_mobile_app/core/components/material_card.dart';
 import 'package:kb_mobile_app/models/agyw_dream.dart';
 
@@ -20,6 +21,10 @@ class _DreamsOutgoingReferralsOutcomeState
     print('Viewing: ${agyw.id}');
   }
 
+  onSearchBeneficiary(String value) {
+    print('Searching: $value');
+  }
+
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
@@ -27,73 +32,78 @@ class _DreamsOutgoingReferralsOutcomeState
       child: Container(
         padding: EdgeInsets.symmetric(vertical: 10.0),
         child: Center(
-          child: Column(
-              children: widget.agywList
-                  .map((agyw) => Container(
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        child: MaterialCard(
-                          body: GestureDetector(
-                            onTap: () => onView(agyw),
-                            child: Container(
-                              child: Column(
-                                children: [
-                                  Container(
-                                    margin: EdgeInsets.symmetric(
-                                        horizontal: 10, vertical: 10),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Expanded(
-                                          flex: 1,
+          child: Column(children: [
+            SearchInput(
+              onSearch: (value) => onSearchBeneficiary(value),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ...widget.agywList
+                .map((agyw) => Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      child: MaterialCard(
+                        body: GestureDetector(
+                          onTap: () => onView(agyw),
+                          child: Container(
+                            child: Column(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 10),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        flex: 1,
+                                        child: Container(
                                           child: Container(
-                                            child: Container(
-                                                padding:
-                                                    EdgeInsets.only(right: 5.0),
-                                                height: 20.0,
-                                                width: 20.0,
-                                                child: SvgPicture.asset(
-                                                  svgIcon,
-                                                )),
-                                          ),
+                                              padding:
+                                                  EdgeInsets.only(right: 5.0),
+                                              height: 20.0,
+                                              width: 20.0,
+                                              child: SvgPicture.asset(
+                                                svgIcon,
+                                              )),
                                         ),
-                                        Expanded(
-                                            flex: 10,
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.start,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Wrap(children: [
-                                                  Text(agyw.toString(),
-                                                      style: TextStyle()
-                                                          .copyWith(
-                                                              color: Color(
-                                                                  0xFF05131B),
-                                                              fontSize: 14.0,
-                                                              fontWeight:
-                                                                  FontWeight
-                                                                      .bold)),
-                                                ]),
-                                                Wrap(children: [
-                                                  Text(agyw.primaryUIC)
-                                                ])
-                                              ],
-                                            )),
-                                      ],
-                                    ),
+                                      ),
+                                      Expanded(
+                                          flex: 10,
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Wrap(children: [
+                                                Text(agyw.toString(),
+                                                    style: TextStyle().copyWith(
+                                                        color:
+                                                            Color(0xFF05131B),
+                                                        fontSize: 14.0,
+                                                        fontWeight:
+                                                            FontWeight.bold)),
+                                              ]),
+                                              Wrap(children: [
+                                                Text(agyw.primaryUIC)
+                                              ])
+                                            ],
+                                          )),
+                                    ],
                                   ),
-                                  SizedBox(
-                                    height: 5,
-                                  )
-                                ],
-                              ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                )
+                              ],
                             ),
                           ),
                         ),
-                      ))
-                  .toList()),
+                      ),
+                    ))
+                .toList()
+          ]),
         ),
       ),
     );
