7e800f1dfcb0a541fb96e82dcbcdfaedf8215dd2
Joseph Chingalo <profschingalo@gmail.com>
2021-06-17 13:36:38 +0300

    feat: service support for offline relogin for a given user

--- lib/core/services/user_service.dart
+++ lib/core/services/user_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:kb_mobile_app/core/offline_db/program_ou_offline/program_ou_offline_provider.dart';
 import 'package:kb_mobile_app/core/offline_db/user_offline/user_offline_provider.dart';
 import 'package:kb_mobile_app/core/offline_db/user_offline/user_ou_offline_provider.dart';
@@ -10,46 +11,53 @@ import 'package:kb_mobile_app/models/current_user.dart';
 class UserService {
   final String preferenceKey = 'currrent_user';
 
-  Future<dynamic> login(
-    String username,
-    String password,
-  ) async {
+  Future<CurrentUser> login({
+    @required String username,
+    @required String password,
+    isOnlineAthentication = true,
+  }) async {
+    CurrentUser user;
     try {
-      var url = 'api/me.json';
-      var queryParameters = {
-        "fields":
-            "id,name,email,phoneNumber,programs,organisationUnits[id],attributeValues[value,attribute[id,name]],userGroups[name],userCredentials[userRoles[name]]"
-      };
-      HttpService http = new HttpService(
-        username: username,
-        password: password,
-      );
-      var response = await http.httpGet(url, queryParameters: queryParameters);
-      return response.statusCode == 200
-          ? CurrentUser.fromJson(
-              json.decode(response.body),
-              username,
-              password,
-            )
-          : null;
+      if (isOnlineAthentication) {
+        var url = 'api/me.json';
+        var queryParameters = {
+          "fields":
+              "id,name,email,phoneNumber,programs,organisationUnits[id],attributeValues[value,attribute[id,name]],userGroups[name],userCredentials[userRoles[name]]"
+        };
+        HttpService http = new HttpService(
+          username: username,
+          password: password,
+        );
+        var response =
+            await http.httpGet(url, queryParameters: queryParameters);
+        if (response.statusCode == 200) {
+          user = CurrentUser.fromJson(
+            json.decode(response.body),
+            username,
+            password,
+          );
+        }
+      } else {
+        List<CurrentUser> users = await UserOfflineProvider().getUsers();
+        try {
+          user = users.firstWhere((CurrentUser user) =>
+              user.username == username && user.password == password);
+        } catch (error) {
+          String message =
+              "You can not login offline with provided username and password, kindly connect to network and try again";
+          throw message;
+        }
+      }
+      return user;
     } catch (error) {
       throw error;
     }
   }
 
   Future logout() async {
-    //@TODO handling offline issues here
-    // password
-    // clearing of database
     CurrentUser user = await getCurrentUser();
     if (user != null) {
       user.isLogin = false;
-      user.password = '';
-      user.userOrgUnitIds = [];
-      user.programs = [];
-      await UserOuOfflineProvider().deleteUserOrganisationUnits(user.id);
-      await UserProgramOfflineProvider().deleteUserPrograms(user.id);
-      await ProgramOuOfflineProvider().clearProgramOrganisationUnits();
       await setCurrentUser(user);
     }
   }
@@ -60,6 +68,12 @@ class UserService {
     List<CurrentUser> filteredUsers =
         users.where((CurrentUser user) => user.id == userId).toList();
     return filteredUsers.length > 0 ? filteredUsers[0] : null;
+  }
+
+  resetUserAssociatedMetadata(String userId) async {
+    await UserOuOfflineProvider().deleteUserOrganisationUnits(userId);
+    await UserProgramOfflineProvider().deleteUserPrograms(userId);
+    await ProgramOuOfflineProvider().clearProgramOrganisationUnits();
   }
 
   setCurrentUser(
