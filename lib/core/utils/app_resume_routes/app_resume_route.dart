697d97a2f1fc15b3a469d5be3d2ce50249672cfb
Joseph Chingalo <profschingalo@gmail.com>
2021-07-05 18:02:23 +0300

    feat: routes with confirmation as well as redirect for ogac module

--- lib/core/utils/app_resume_routes/app_resume_route.dart
+++ lib/core/utils/app_resume_routes/app_resume_route.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:kb_mobile_app/core/utils/app_resume_routes/dreams_routes/dreams_enrollment_route.dart';
 import 'package:kb_mobile_app/core/utils/app_resume_routes/dreams_routes/dreams_services_route.dart';
 import 'package:kb_mobile_app/core/utils/app_resume_routes/ogac_routes/ogac_enrollment_route.dart';
 import 'package:kb_mobile_app/core/utils/app_resume_routes/ovc_routes/ovc_enrollment_route.dart';
 import 'package:kb_mobile_app/core/utils/app_resume_routes/ovc_routes/ovc_services_route.dart';
+import 'package:kb_mobile_app/core/utils/app_util.dart';
+import 'package:kb_mobile_app/models/form_auto_save.dart';
+import 'package:kb_mobile_app/modules/ogac_intervention/constants/ogac_intervention_constant.dart';
 
 class AppResumeRoute
     with
@@ -11,6 +15,95 @@ class AppResumeRoute
         OgacEnrollmentRoute,
         OvcEnrollmentRoute,
         OvcServicesRoute {
-  //@TODO logics for checking all routes resumes
+  void redirectToPages(
+    BuildContext context,
+    FormAutoSave formAutoSave,
+  ) {
+    if (formAutoSave.nextPageModule ==
+        OgacInterventionConstant.nextPageModule) {
+      redirectToOgacEnrollmemntForm(context, formAutoSave);
+    }
+  }
 
+  Future<bool> shouldResumeWithUnSavedChanges(
+    BuildContext context,
+    FormAutoSave formAutoSave,
+  ) async {
+    bool diablePadding = false;
+    bool shouldResumeFormState = false;
+    if (formAutoSave.hasFormAutoSaveData()) {
+      String pageModule = formAutoSave.pageModule;
+      Widget modal = getConfirmationWidget(pageModule, context);
+      dynamic hasConfirmResume =
+          await AppUtil.showPopUpModal(context, modal, diablePadding);
+      shouldResumeFormState = "$hasConfirmResume" == "true";
+    }
+    return shouldResumeFormState;
+  }
+
+  Widget getConfirmationWidget(String pageModule, BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 10.0,
+      ),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 10.0,
+            ),
+            child: Text(
+              "You have unsaved data on $pageModule module, Do want to continue?",
+              style: TextStyle().copyWith(),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(
+              vertical: 10.0,
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                    ),
+                    child: MaterialButton(
+                      color: Colors.greenAccent,
+                      onPressed: () => Navigator.pop(context, true),
+                      child: Container(
+                        child: Text(
+                          "Continue",
+                          style: TextStyle().copyWith(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                    ),
+                    child: MaterialButton(
+                      color: Colors.redAccent,
+                      onPressed: () => Navigator.pop(context),
+                      child: Container(
+                        child: Text(
+                          "Discard changes",
+                          style: TextStyle().copyWith(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/core/utils/app_resume_routes/ogac_routes/ogac_enrollment_route.dart
+++ lib/core/utils/app_resume_routes/ogac_routes/ogac_enrollment_route.dart
@@ -1,3 +1,32 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/app_state/enrollment_service_form_state/enrollment_form_state.dart';
+import 'package:kb_mobile_app/models/form_auto_save.dart';
+import 'package:kb_mobile_app/modules/ogac_intervention/pages/ogac_enrollment_form.dart';
+import 'package:provider/provider.dart';
+
 class OgacEnrollmentRoute {
-  //@TODO implement routes for ogac enrollment
+  redirectToOgacEnrollmemntForm(
+    BuildContext context,
+    FormAutoSave formAutoSave,
+  ) {
+    setEnrollmentFormState(context, formAutoSave);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return OgacEnrollemntForm();
+        },
+      ),
+    );
+  }
+
+  void setEnrollmentFormState(BuildContext context, FormAutoSave formAutoSave) {
+    Provider.of<EnrollmentFormState>(context, listen: false).resetFormState();
+    Map<String, dynamic> dataObject = jsonDecode(formAutoSave.data);
+    for (String id in dataObject.keys) {
+      Provider.of<EnrollmentFormState>(context, listen: false)
+          .setFormFieldState(id, dataObject[id]);
+    }
+  }
 }
