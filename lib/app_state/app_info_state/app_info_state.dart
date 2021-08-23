bca9e1ec1caff17f4fac51b7dffb8c6308cf28f1
Joseph Chingalo <profschingalo@gmail.com>
2021-08-23 14:45:50 +0300

    fix: contents for about us page

--- lib/app_state/app_info_state/app_info_state.dart
+++ lib/app_state/app_info_state/app_info_state.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/foundation.dart';
+import 'package:kb_mobile_app/core/constants/app_info_reference.dart';
 import 'package:new_version/new_version.dart';
 
 class AppInfoState with ChangeNotifier {
   String? _currentAppName;
   String? _currentAppVersion;
   String? _currentAppId;
-  String? _currentPlatformVersion;
+  bool? _canUpdate;
 
   String get currentAppName => _currentAppName ?? '';
+  bool get canUpdate => _canUpdate ?? false;
   String get currentAppVersion => _currentAppVersion ?? '';
   String get currentAppId => _currentAppId ?? '';
-  String get currentPlatformVersion => _currentPlatformVersion ?? '';
 
   void setCurrentAppInfo() async {
     try {
-      //@TODO add contansts
       final newVersion = NewVersion(
-        androidId: 'chingalo.family.todo_app',
+        androidId: AppInfoReference.androidId,
       );
       final status = await newVersion.getVersionStatus();
-      print("${status!.canUpdate}");
-      print("${status.localVersion}");
-      print("${status.storeVersion}");
-      print("${status.releaseNotes}");
-      // TODO add a way to get the app info from the platform
-      _currentAppId = '';
-      _currentAppVersion = '';
-      _currentAppName = '';
-      _currentPlatformVersion = '';
+      _canUpdate = status!.canUpdate;
+      _currentAppVersion = status.localVersion;
+      _currentAppId = AppInfoReference.androidId;
+      _currentAppName = AppInfoReference.currentAppName;
     } catch (error) {}
     notifyListeners();
   }
