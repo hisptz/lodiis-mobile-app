de064430c178e07c2a470075f9f57bc1497e2c8f
Joseph Chingalo <profschingalo@gmail.com>
2021-09-01 10:43:36 +0300

    feat: support for getting location info for referral events

--- lib/models/referral_event.dart
+++ lib/models/referral_event.dart
@@ -1,15 +1,25 @@
+import 'package:kb_mobile_app/core/constants/user_account_reference.dart';
+import 'package:kb_mobile_app/core/services/organisation_unit_service.dart';
 import 'package:kb_mobile_app/models/events.dart';
+import 'package:kb_mobile_app/models/organisation_unit.dart';
 
 class ReferralEvent {
   //@TODO Add location where service has been referred to
   // @TODO add IP who has issued referrals
   //@TODO add Ip where referrels has issues to
+
+  // ['jOXN2iPhkxj','ubB83OWNWsv]=> locations to referals
+  // referrece to => y0bvausyTyh
+  // referal from => UserAccountReference.implementingPartnerDataElement
   String? id;
   String? date;
   String? serviceMode;
   String? type;
   String? category;
   String? status;
+  String? location;
+  String? referredFromIp;
+  String? referredToIp;
   String? comments;
   Events? eventData;
 
@@ -19,14 +29,17 @@ class ReferralEvent {
     this.serviceMode,
     this.category,
     this.status,
+    this.location,
+    this.referredFromIp,
+    this.referredToIp,
     this.type,
     this.comments,
     this.eventData,
   });
 
-  ReferralEvent fromTeiModel(
+  Future<ReferralEvent> fromTeiModel(
     Events eventData,
-  ) {
+  ) async {
     List keys = [
       'qAed23reDPP',
       'AuCryxQYmrk',
@@ -34,19 +47,28 @@ class ReferralEvent {
       'iifSkIcBZz2',
       'LLWTHwhnch0',
       'rsh5Kvx6qAU',
-      'WHktsYoFqat'
+      'WHktsYoFqat',
+      'jOXN2iPhkxj',
+      'ubB83OWNWsv',
+      'y0bvausyTyh',
+      UserAccountReference.implementingPartnerDataElement
     ];
-    Map data = Map();
+    Map<String, dynamic> data = Map();
     for (Map detailObj in eventData.dataValues) {
       String? attribute = detailObj['dataElement'];
       if (attribute != null && keys.indexOf(attribute) > -1) {
         data[attribute] = '${detailObj['value']}'.trim();
       }
     }
+    String location = await getLocationByIds(data);
     return ReferralEvent(
       id: eventData.event,
       date: eventData.eventDate,
       serviceMode: data['qAed23reDPP'] ?? '',
+      referredToIp: data['y0bvausyTyh'] ?? '',
+      referredFromIp:
+          data[UserAccountReference.implementingPartnerDataElement] ?? '',
+      location: location,
       type: data['OrC9Bh2bcFz'] != null && data['OrC9Bh2bcFz'] != ''
           ? data['OrC9Bh2bcFz']
           : data['rsh5Kvx6qAU'] != null && data['rsh5Kvx6qAU'] != ""
@@ -65,6 +87,29 @@ class ReferralEvent {
       status: '',
       eventData: eventData,
     );
+  }
+
+  Future<String> getLocationByIds(Map<String, dynamic> data) async {
+    String location = "";
+    try {
+      List referenceIds = ['jOXN2iPhkxj', 'ubB83OWNWsv'];
+      List ouIds = referenceIds
+          .map((id) => data[id] ?? "")
+          .toList()
+          .where((id) => id != '')
+          .toSet()
+          .toList();
+      print("$referenceIds => $ouIds");
+      if (ouIds.isNotEmpty) {
+        List<OrganisationUnit> ous =
+            await OrganisationUnitService().getOrganisationUnits(ouIds);
+        print(ous);
+      }
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+
+    return location;
   }
 
   @override
