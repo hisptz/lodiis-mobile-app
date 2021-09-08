73e407dfc4924e1e7ed29d7988f6a7fc44d9cea9
Joseph Chingalo <profschingalo@gmail.com>
2021-09-08 15:49:24 +0300

    feat: support for display user access to data entry warning

--- lib/modules/intervention_selection/intervention_selection.dart
+++ lib/modules/intervention_selection/intervention_selection.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/core/components/access_to_data_entry_warning.dart';
 import 'package:provider/provider.dart';
 import 'package:kb_mobile_app/app_state/current_user_state/current_user_state.dart';
 import 'package:kb_mobile_app/app_state/dreams_intervention_list_state/dreams_intervention_list_state.dart';
@@ -64,54 +65,67 @@ class _InterventionSelectionState extends State<InterventionSelection> {
             Container(
               decoration: BoxDecoration(color: primaryColor),
             ),
-            Container(child: Consumer<OvcInterventionListState>(
-              builder: (context, ovcInterventionListState, child) {
-                bool isOvcListLoading = ovcInterventionListState.isLoading;
-                int numberOfHouseholds =
-                    ovcInterventionListState.numberOfHouseholds;
-                int numberOfOvcs = ovcInterventionListState.numberOfOvcs;
-                return Container(
-                  child: Consumer<DreamsInterventionListState>(
-                    builder: (context, dreamsInterventionListState, child) {
-                      bool isDreamsListLoading =
-                          dreamsInterventionListState.isLoading;
-                      int numberOfAgywDreamsBeneficiaries =
-                          dreamsInterventionListState
-                              .numberOfAgywDreamsBeneficiaries;
-                      int numberOfNoneAgywDreamsBeneficiaries =
-                          dreamsInterventionListState
-                              .numberOfNoneAgywDreamsBeneficiaries;
-                      return Consumer<OgacInterventionListState>(
-                        builder: (context, ogacInterventionListState, child) {
-                          int numberOfOgac =
-                              ogacInterventionListState.numberOfOgac;
-                          bool isOgacListLoading =
-                              ogacInterventionListState.isLoading;
-                          return Container(
-                            child: isDreamsListLoading ||
-                                    isOvcListLoading ||
-                                    isOgacListLoading
-                                ? CircularProcessLoader()
-                                : InterventionSelectionContainer(
-                                    interventionPrograms: interventionPrograms,
-                                    onInterventionSelection:
-                                        onInterventionSelection,
-                                    numberOfHouseholds: numberOfHouseholds,
-                                    numberOfAgywDreamsBeneficiaries:
-                                        numberOfAgywDreamsBeneficiaries,
-                                    numberOfNoneAgywDreamsBeneficiaries:
-                                        numberOfNoneAgywDreamsBeneficiaries,
-                                    numberOfOvcs: numberOfOvcs,
-                                    numberOfOgac: numberOfOgac,
-                                  ),
+            Container(
+              child: Consumer<OvcInterventionListState>(
+                builder: (context, ovcInterventionListState, child) {
+                  bool isOvcListLoading = ovcInterventionListState.isLoading;
+                  int numberOfHouseholds =
+                      ovcInterventionListState.numberOfHouseholds;
+                  int numberOfOvcs = ovcInterventionListState.numberOfOvcs;
+                  return Consumer<CurrentUserState>(
+                      builder: (context, currentUserState, child) {
+                    bool hasAccessToDataEntry =
+                        currentUserState.canCurrentUserDoDataEntry;
+                    return !hasAccessToDataEntry
+                        ? AccessToDataEntryWarning()
+                        : Container(
+                            child: Consumer<DreamsInterventionListState>(
+                              builder: (context, dreamsInterventionListState,
+                                  child) {
+                                bool isDreamsListLoading =
+                                    dreamsInterventionListState.isLoading;
+                                int numberOfAgywDreamsBeneficiaries =
+                                    dreamsInterventionListState
+                                        .numberOfAgywDreamsBeneficiaries;
+                                int numberOfNoneAgywDreamsBeneficiaries =
+                                    dreamsInterventionListState
+                                        .numberOfNoneAgywDreamsBeneficiaries;
+                                return Consumer<OgacInterventionListState>(
+                                  builder: (context, ogacInterventionListState,
+                                      child) {
+                                    int numberOfOgac =
+                                        ogacInterventionListState.numberOfOgac;
+                                    bool isOgacListLoading =
+                                        ogacInterventionListState.isLoading;
+                                    return Container(
+                                      child: isDreamsListLoading ||
+                                              isOvcListLoading ||
+                                              isOgacListLoading
+                                          ? CircularProcessLoader()
+                                          : InterventionSelectionContainer(
+                                              interventionPrograms:
+                                                  interventionPrograms,
+                                              onInterventionSelection:
+                                                  onInterventionSelection,
+                                              numberOfHouseholds:
+                                                  numberOfHouseholds,
+                                              numberOfAgywDreamsBeneficiaries:
+                                                  numberOfAgywDreamsBeneficiaries,
+                                              numberOfNoneAgywDreamsBeneficiaries:
+                                                  numberOfNoneAgywDreamsBeneficiaries,
+                                              numberOfOvcs: numberOfOvcs,
+                                              numberOfOgac: numberOfOgac,
+                                            ),
+                                    );
+                                  },
+                                );
+                              },
+                            ),
                           );
-                        },
-                      );
-                    },
-                  ),
-                );
-              },
-            )),
+                  });
+                },
+              ),
+            ),
           ],
         ),
       ),
