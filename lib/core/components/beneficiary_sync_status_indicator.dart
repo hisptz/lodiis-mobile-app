24381b3ad44d4298c8b334663b1d12d17859e468
Joseph Chingalo <profschingalo@gmail.com>
2021-09-10 12:37:52 +0300

    feat(sync indicator): support for component for indicate sync status

---
+++ lib/core/components/beneficiary_sync_status_indicator.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class BeneficiarySyncStatusIndicator extends StatelessWidget {
+  const BeneficiarySyncStatusIndicator({
+    Key? key,
+    required this.iconHeight,
+    required this.isSynced,
+    this.onTap,
+  }) : super(key: key);
+
+  final double iconHeight;
+  final bool isSynced;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+        onTap: () => {},
+        child: Container(
+          height: iconHeight,
+          width: iconHeight,
+          margin: EdgeInsets.symmetric(
+            vertical: 5.0,
+            horizontal: 5.0,
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/sync.svg',
+            color: isSynced == true ? Colors.green : Colors.amber,
+          ),
+        ),
+      ),
+    );
+  }
+}
