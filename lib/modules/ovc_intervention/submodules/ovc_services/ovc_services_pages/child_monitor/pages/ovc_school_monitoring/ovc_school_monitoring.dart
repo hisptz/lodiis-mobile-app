70692a8df4bc4a8e658ac9fc186a1245f3a11f14
chingaloEric <echingalo@gmail.com>
2021-09-29 16:45:27 +0300

    refactor(ovc_child_monitor): updated the ui for ovc service monitor

---
+++ lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/child_monitor/pages/ovc_school_monitoring/ovc_school_monitoring.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/app_state/enrollment_service_form_state/service_event_data_state.dart';
+import 'package:kb_mobile_app/app_state/enrollment_service_form_state/service_form_state.dart';
+import 'package:kb_mobile_app/core/components/circular_process_loader.dart';
+import 'package:kb_mobile_app/core/components/entry_form_save_button.dart';
+import 'package:kb_mobile_app/core/utils/tracked_entity_instance_util.dart';
+import 'package:kb_mobile_app/models/events.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/child_monitor/components/ovc_child_monitor_home_list_container.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/child_monitor/pages/ovc_school_monitoring/constants/ovc_school_monitoring_constant.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/child_monitor/pages/ovc_school_monitoring/pages/ovc_school_monitoring_form.dart';
+import 'package:provider/provider.dart';
+
+class OvcSchoolMonitoring extends StatefulWidget {
+  const OvcSchoolMonitoring({Key? key}) : super(key: key);
+
+  @override
+  _OvcSchoolMonitoringState createState() => _OvcSchoolMonitoringState();
+}
+
+class _OvcSchoolMonitoringState extends State<OvcSchoolMonitoring> {
+  final List<String> programStageIds = [
+    OvcSchoolMonitoringConstant.programStage
+  ];
+
+  void onAddSchoolMonitoring(BuildContext context) {
+    Provider.of<ServiceFormState>(context, listen: false).resetFormState();
+    Provider.of<ServiceFormState>(context, listen: false)
+        .updateFormEditabilityState(isEditableMode: true);
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => OvcSchoolMonitoringForm()));
+  }
+
+  void updateFormStateData(BuildContext context, Events eventData) {
+    Provider.of<ServiceFormState>(context, listen: false)
+        .setFormFieldState('eventDate', eventData.eventDate);
+    Provider.of<ServiceFormState>(context, listen: false)
+        .setFormFieldState('eventId', eventData.event);
+    for (Map dataValue in eventData.dataValues) {
+      if (dataValue['value'] != '') {
+        Provider.of<ServiceFormState>(context, listen: false)
+            .setFormFieldState(dataValue['dataElement'], dataValue['value']);
+      }
+    }
+  }
+
+  void onEditSchoolMonitoring(
+    BuildContext context,
+    String? monitorResponse,
+    Events eventData,
+  ) {
+    bool isEditableMode = true;
+    updateFormStateData(context, eventData);
+    Provider.of<ServiceFormState>(context, listen: false)
+        .updateFormEditabilityState(isEditableMode: isEditableMode);
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => OvcSchoolMonitoringForm()));
+  }
+
+  void onViewSchoolMonitoring(
+    BuildContext context,
+    String? monitorResponse,
+    Events eventData,
+  ) {
+    bool isEditableMode = false;
+    updateFormStateData(context, eventData);
+    Provider.of<ServiceFormState>(context, listen: false)
+        .updateFormEditabilityState(isEditableMode: isEditableMode);
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => OvcSchoolMonitoringForm()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ServiceEventDataState>(
+        builder: (context, serviceEventDataState, _) {
+      bool isLoading = serviceEventDataState.isLoading;
+      Map<String?, List<Events>> eventListByProgramStage =
+          serviceEventDataState.eventListByProgramStage;
+
+      List<Events> events = TrackedEntityInstanceUtil
+          .getAllEventListFromServiceDataStateByProgramStages(
+              eventListByProgramStage, programStageIds);
+      int monitoringCount = events.length;
+      return isLoading
+          ? CircularProcessLoader(
+              color: Colors.blueGrey,
+            )
+          : Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 10.0),
+                  child: events.length == 0
+                      ? Text('There is no School Monitoring at a moment')
+                      : Column(
+                          children: events.map((Events event) {
+                            int index = monitoringCount--;
+                            return Container(
+                                margin: EdgeInsets.only(bottom: 15.0),
+                                child: OvcChildSchoolMonitorContainer(
+                                  eventData: event,
+                                  index: index,
+                                  onEditMonitor: () => null,
+                                  onViewMonitor: () => null,
+                                ));
+                          }).toList(),
+                        ),
+                ),
+                EntryFormSaveButton(
+                  label: 'ADD MONITORING',
+                  labelColor: Colors.white,
+                  buttonColor: Color(0xFF4B9F46),
+                  fontSize: 15.0,
+                  onPressButton: () => onAddSchoolMonitoring(context),
+                )
+              ],
+            );
+    });
+  }
+}
---
+++ lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/child_monitor/pages/ovc_service_monitoring/ovc_service_monitoring.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OvcServiceMonitoring extends StatefulWidget {
+  const OvcServiceMonitoring({Key? key}) : super(key: key);
+
+  @override
+  _OvcServiceMonitoringState createState() => _OvcServiceMonitoringState();
+}
+
+class _OvcServiceMonitoringState extends State<OvcServiceMonitoring> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('OvcServiceMonitoring'),
+      ),
+    );
+  }
+}
