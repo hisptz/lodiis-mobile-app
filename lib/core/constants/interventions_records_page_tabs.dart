5c01643d6c1303e55dd908d6c898ff0696c06ca0
chingaloEric <echingalo@gmail.com>
2021-10-03 14:06:59 +0300

    feat(ovc_records): added enrollments page on the records page

--- lib/core/constants/interventions_records_page_tabs.dart
+++ lib/core/constants/interventions_records_page_tabs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/modules/ovc_intervention/submodules/ovc_records/pages/ovc_enrollment_records.dart';
 
 class InterventionRecordsPageTabs {
   static final Map<String, Widget> dreamsModule = {
@@ -11,8 +12,7 @@ class InterventionRecordsPageTabs {
         child: Text('No Enrollment Criteria')),
   };
   static final Map<String, Widget> ovcModule = {
-    'Households':
-        Container(margin: EdgeInsets.only(top: 20), child: Text('Households')),
+    'Households': OvcEnrollmentRecords(),
     'None Participation': Container(
         margin: EdgeInsets.only(top: 20), child: Text('None Participation')),
   };
--- lib/modules/ovc_intervention/submodules/ovc_records/ovc_records_page.dart
+++ lib/modules/ovc_intervention/submodules/ovc_records/ovc_records_page.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:kb_mobile_app/app_state/language_translation_state/language_translation_state.dart';
-import 'package:kb_mobile_app/app_state/ovc_intervention_list_state/ovc_intervention_list_state.dart';
-import 'package:kb_mobile_app/core/components/paginated_list_view.dart';
-import 'package:kb_mobile_app/core/components/sub_module_home_container.dart';
-import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_household_card.dart';
-import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_household_card_body.dart';
-import 'package:kb_mobile_app/modules/ovc_intervention/components/ovc_household_card_botton_content.dart';
-import 'package:provider/provider.dart';
 
 class OvcRecordsPage extends StatefulWidget {
   const OvcRecordsPage({
@@ -23,94 +15,14 @@ class OvcRecordsPage extends StatefulWidget {
 }
 
 class _OvcRecordsPageState extends State<OvcRecordsPage> {
-  final bool canEdit = false;
-  final bool canView = true;
-  final bool canExpand = true;
-  final bool canAddChild = false;
-  final bool canViewChildInfo = true;
-  final bool canEditChildInfo = false;
-  final bool canViewChildService = false;
-  final bool canViewChildReferral = false;
-  final bool canViewChildExit = false;
-
   String? toggleCardId = '';
-
-  void onCardToggle(String? cardId) {
-    setState(() {
-      toggleCardId = canExpand && cardId != toggleCardId ? cardId : '';
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Consumer<LanguageTranslationState>(
-        builder: (context, languageTranslationState, child) {
-          String? currentLanguage = languageTranslationState.currentLanguage;
-          return Consumer<OvcInterventionListState>(
-            builder: (context, ovcInterventionListState, child) {
-              String header = currentLanguage == 'lesotho'
-                  ? 'Lethathamo la malapa'.toUpperCase() +
-                      ': ${ovcInterventionListState.numberOfHouseholds} Malapa'
-                  : 'Household list'.toUpperCase() +
-                      ': ${ovcInterventionListState.numberOfHouseholds} households';
-              return SubModuleHomeContainer(
-                header: header,
-                bodyContents: _buildBody(currentLanguage),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildBody(String? currentLanguage) {
     return Scaffold(
         body: TabBarView(
       controller: widget.tabsController,
       children: widget.tabsVieItems.map((Widget tab) => tab).toList(),
     ));
-    // return Consumer<OvcInterventionListState>(
-    //     builder: (context, ovcListState, child) => CustomPaginatedListView(
-    //         childBuilder: (context, ovcHousehold, index) => OvcHouseholdCard(
-    //               ovcHousehold: ovcHousehold,
-    //               canEdit: canEdit,
-    //               canExpand: canExpand,
-    //               canView: canView,
-    //               isExpanded: ovcHousehold.id == toggleCardId,
-    //               onCardToggle: () {
-    //                 onCardToggle(ovcHousehold.id);
-    //               },
-    //               cardBody: OvcHouseholdCardBody(
-    //                 ovcHousehold: ovcHousehold,
-    //               ),
-    //               cardButtonActions: Container(),
-    //               cardButtonContent: OvcHouseholdCardButtonContent(
-    //                 currentLanguage: currentLanguage,
-    //                 ovcHousehold: ovcHousehold,
-    //                 canAddChild: canAddChild,
-    //                 canViewChildInfo: canViewChildInfo,
-    //                 canEditChildInfo: canEditChildInfo,
-    //                 canViewChildService: canViewChildService,
-    //                 canViewChildReferral: canViewChildReferral,
-    //                 canViewChildExit: canViewChildExit,
-    //               ),
-    //             ),
-    //         pagingController: ovcListState.pagingController,
-    //         emptyListWidget: Center(
-    //           child: Text(
-    //             currentLanguage == 'lesotho'
-    //                 ? 'Ha hona lelapa le ngolisitsoeng ha hajoale'
-    //                 : 'There is no household enrolled at moment',
-    //           ),
-    //         ),
-    //         errorWidget: Center(
-    //           child: Text(
-    //             currentLanguage == 'lesotho'
-    //                 ? 'Ha hona lelapa le ngolisitsoeng ha hajoale'
-    //                 : 'There is no household enrolled at moment',
-    //           ),
-    //         )));
   }
 }
