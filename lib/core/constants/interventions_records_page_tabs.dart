9f4ce52a70228104c79b7e0107d9a8fcc92e24e4
chingaloEric <echingalo@gmail.com>
2021-10-13 09:10:07 +0300

    feat(agyw-dreams): added support for KB prep records

--- lib/core/constants/interventions_records_page_tabs.dart
+++ lib/core/constants/interventions_records_page_tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_records/pages/beneficiaries_without_dreams_enrollment_criteria_records_page.dart';
 import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_records/pages/dreams_enrollment_records.dart';
 import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_records/pages/dreams_none_participation_records.dart';
+import 'package:kb_mobile_app/modules/dreams_intervention/submodules/dreams_records/pages/non_agyw_records.dart';
 import 'package:kb_mobile_app/modules/education_intervention/submodules/education_records/pages/bursary_records_page.dart';
 import 'package:kb_mobile_app/modules/education_intervention/submodules/education_records/pages/bursary_without_vulnerability_records_page.dart';
 import 'package:kb_mobile_app/modules/education_intervention/submodules/education_records/pages/lbse_records_page.dart';
@@ -20,9 +21,9 @@ class InterventionRecordsPageTabs {
     'No Enrollment Criteria': InterventionRecordsPageTabs(
         page: BeneficiariesWithoutDreamsEnrollmentCriteriaRecordsPage(),
         implementingPartners: []),
-    // 'Kb PrEP': InterventionRecordsPageTabs(
-    //     page: BeneficiariesWithoutDreamsEnrollmentCriteriaRecordsPage(),
-    //     implementingPartners: ['KB-AGYW/DREAMS', 'Super user'])
+    'Kb PrEP': InterventionRecordsPageTabs(
+        page: NonAgywRecords(),
+        implementingPartners: ['KB-AGYW/DREAMS', 'Super user'])
   };
   static final Map<String, InterventionRecordsPageTabs> ovcModule = {
     'Households': InterventionRecordsPageTabs(page: OvcEnrollmentRecords()),
