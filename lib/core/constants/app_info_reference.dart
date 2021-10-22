5633d3ab1014143fcfb00a79ccb78bf1ccd6061d
Joseph Chingalo <profschingalo@gmail.com>
2021-10-22 12:49:48 +0300

    chore: support for backup app refererence for app info

--- lib/core/constants/app_info_reference.dart
+++ lib/core/constants/app_info_reference.dart
@@ -1,4 +1,5 @@
 class AppInfoReference {
   static final String androidId = 'org.hisptanzania.kb_mobile_app';
   static final String currentAppName = '[TEST] Lesotho OVC-DREAMS';
+  static final String currentAppVersion = '1.3.0';
 }
