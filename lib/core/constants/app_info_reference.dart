4592d45eb6ba0bfa6615874dfc287a5309101d77
Joseph Chingalo <profschingalo@gmail.com>
2021-10-28 14:40:35 +0300

    chore: prepare app version for preleases v2.0

--- lib/core/constants/app_info_reference.dart
+++ lib/core/constants/app_info_reference.dart
@@ -1,7 +1,7 @@
 class AppInfoReference {
   static final String androidId = 'org.hisptanzania.kb_mobile_app';
   static final String currentAppName = '[TEST] Lesotho OVC-DREAMS';
-  static final String currentAppVersion = '1.3.1';
+  static final String currentAppVersion = '2.0.0-rc-0';
   static final int minmunAllowedMajorVersion = 0;
   static final int minmunAllowedMinorVersion = 0;
   static final int minmunAllowedPatchVersion = 2;
