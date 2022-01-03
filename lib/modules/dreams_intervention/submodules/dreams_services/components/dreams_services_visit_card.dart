7505df6f0e9d907564be0b1bdf5fbf18d6eb0d4b
Joseph Chingalo <profschingalo@gmail.com>
2022-01-03 12:27:07 +0300

    feat: support for restriction for services for pep,prep,service forms,art,condoms,anc,contraceptives

--- lib/modules/dreams_intervention/submodules/dreams_services/components/dreams_services_visit_card.dart
+++ lib/modules/dreams_intervention/submodules/dreams_services/components/dreams_services_visit_card.dart
@@ -86,22 +86,27 @@ class DreamsServiceVisitCard extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 5.0,
-                    ),
-                    child: InkWell(
-                      onTap: editDisabled == true ? null : onEdit as void Function()?,
-                      child: Container(
-                        height: iconHeight,
-                        width: iconHeight,
-                        margin: EdgeInsets.symmetric(
-                          vertical: 5,
-                          horizontal: 5,
-                        ),
-                        child: SvgPicture.asset(
-                          'assets/icons/edit-icon.svg',
-                          color: Color(0xFF1F8ECE),
+                  Visibility(
+                    visible: eventData.enrollmentOuAccessible!,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 5.0,
+                      ),
+                      child: InkWell(
+                        onTap: editDisabled == true
+                            ? null
+                            : onEdit as void Function()?,
+                        child: Container(
+                          height: iconHeight,
+                          width: iconHeight,
+                          margin: EdgeInsets.symmetric(
+                            vertical: 5,
+                            horizontal: 5,
+                          ),
+                          child: SvgPicture.asset(
+                            'assets/icons/edit-icon.svg',
+                            color: Color(0xFF1F8ECE),
+                          ),
                         ),
                       ),
                     ),
