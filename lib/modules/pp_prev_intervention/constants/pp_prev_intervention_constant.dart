8d32c4b552f3e2873b501edc47841bd96dd1ac20
Joseph Chingalo <profschingalo@gmail.com>
2022-01-05 14:33:46 +0300

    chore: adding constants for default colors and allowed level for service provision locations

--- lib/modules/pp_prev_intervention/constants/pp_prev_intervention_constant.dart
+++ lib/modules/pp_prev_intervention/constants/pp_prev_intervention_constant.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
+
 class PpPrevInterventionConstant {
   static final String program = 'iR6hbkQABMk';
   static final String trackedEntityType = 'XZIKX0bA8WN';
   static final String programStage = 'aH149rQ8eco';
+  static final List<int> allowedSelectedLevels = [3];
+  static final Color inputColor = Color(0xFF9B2BAE);
+  static final Color labelColor = Color(0xFF737373);
 }
