dad667382c39ae10c6f062086967143624bc48d9
Joseph Chingalo <profschingalo@gmail.com>
2022-07-06 13:00:30 +0300

    fix: 🐛 app id for testing application version

--- lib/core/constants/app_info_reference.dart
+++ lib/core/constants/app_info_reference.dart
@@ -1,5 +1,5 @@
 class AppInfoReference {
-  static const String androidId = 'org.hisptanzania.kb_mobile_app';
+  static const String androidId = 'org.hisptanzania.kb_mobile_app.testing';
   static const String currentAppName = '[TEST] Lesotho OVC-DREAMS';
   static const String currentAppVersion = '1.4.2';
   static const int minimumAllowedMajorVersion = 0;
