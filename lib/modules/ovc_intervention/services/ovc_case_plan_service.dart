1feab002da2c9b7734c9e74ee2b9105a93ca3bfd
Joseph Chingalo <profschingalo@gmail.com>
2022-08-06 12:31:16 +0300

    feat: 🎸 add services for get case plan data from offline db

---
+++ lib/modules/ovc_intervention/services/ovc_case_plan_service.dart
@@ -0,0 +1,78 @@
+import 'package:kb_mobile_app/core/offline_db/event_offline/event_offline_provider.dart';
+import 'package:kb_mobile_app/models/case_plan_event.dart';
+import 'package:kb_mobile_app/models/case_plan_gap_event.dart';
+import 'package:kb_mobile_app/models/case_plan_gap_service_monitoring_event.dart';
+import 'package:kb_mobile_app/models/case_plan_gap_service_provision_event.dart';
+import 'package:kb_mobile_app/models/events.dart';
+
+class OvcCasePlanService {
+  Future<List<CasePlanEvent>> getCasePlanEvents({
+    required String date,
+    required String programStageId,
+    required String teiId,
+  }) async {
+    List<Events> events =
+        await EventOfflineProvider().getEventByTeiByEventDateByProgramStage(
+      date: date,
+      programStageId: programStageId,
+      teiId: teiId,
+    );
+    return events
+        .map((eventData) => CasePlanEvent().toDataModel(eventData: eventData))
+        .toList();
+  }
+
+  Future<List<CasePlanGapEvent>> getCasePlanGapEvents({
+    required String date,
+    required String programStageId,
+    required String teiId,
+    required List casePlanToGaps,
+  }) async {
+    List<Events> events =
+        await EventOfflineProvider().getEventByTeiByEventDateByProgramStage(
+      date: date,
+      programStageId: programStageId,
+      teiId: teiId,
+    );
+    return events
+        .map(
+            (eventData) => CasePlanGapEvent().toDataModel(eventData: eventData))
+        .toList();
+  }
+
+  Future<List<CasePlanGapServiceProvisionEvent>>
+      getCasePlanServiveProvisonEvents({
+    required String date,
+    required String programStageId,
+    required String teiId,
+  }) async {
+    List<Events> events =
+        await EventOfflineProvider().getEventByTeiByEventDateByProgramStage(
+      date: date,
+      programStageId: programStageId,
+      teiId: teiId,
+    );
+    return events
+        .map((eventData) => CasePlanGapServiceProvisionEvent()
+            .toDataModel(eventData: eventData))
+        .toList();
+  }
+
+  Future<List<CasePlanGapServiceMonitoringEvent>>
+      getCasePlanServiceMonitoringEvents({
+    required String date,
+    required String programStageId,
+    required String teiId,
+  }) async {
+    List<Events> events =
+        await EventOfflineProvider().getEventByTeiByEventDateByProgramStage(
+      date: date,
+      programStageId: programStageId,
+      teiId: teiId,
+    );
+    return events
+        .map((eventData) => CasePlanGapServiceMonitoringEvent()
+            .toDataModel(eventData: eventData))
+        .toList();
+  }
+}
