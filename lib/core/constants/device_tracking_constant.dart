bb80fa256442c8c5d9a22810eb0bfb5dc9fcd1c0
Joseph Chingalo <profschingalo@gmail.com>
2022-08-10 13:52:02 +0300

    feat: 🎸 add device tracking program references

---
+++ lib/core/constants/device_tracking_constant.dart
@@ -0,0 +1,30 @@
+class DeviceTrackingConstant {
+  static String program = 'Jx9LuVZTpvm';
+  static String trackedEntityType = 'OjEuZrFaLkr';
+  //Profile
+  static String deviceId = 'aLN7rCIoe2W';
+  static String deviceModel = 'J9K8nLZ0YOe';
+  static String deviceManufacturerName = 'SZi6olLI78w';
+  static String productName = 'ly9t0Gx0DzB';
+  // device details
+  static String deviceProgramStage = 'PmPYb2AKTA1';
+  static String deviceOs = 'MpE6gCbHMXn';
+  static String securityPatchDate = 'eAgkO332Lq0';
+  //App details
+  static String appDetailProgramStage = 'FQFPDy4QrQi';
+  static String appVersion = 'L8MKlJ6sec6';
+  static String syncedEnrollemnts = 'gLQk37GDUfh';
+  static String unSyncedEnrollemnts = 'sx0TRUaI5u5';
+  static String syncedServices = 'LRZDPPav45g';
+  static String unSyncedServices = 'G7c2qpYWeuW';
+  //synchronization
+  static String syncInfoProgramStage = 's48JSKcOrje';
+  static String dataStoreRef = 'eaF7jMjJuvB';
+  static String lastDataDownloadDate = 'yrpuDa3tbEe';
+  static String lastDataUploadDate = 'xc3LT9bsieK';
+  static String lastMetadataDownloadDate = 'OOsw7QMykr7';
+  //user acount
+  static String userAccountProgramStage = 'IrRK9ULDxY1';
+  static String username = 'jbJP3BtcGbh';
+  static String lastLoginDate = 'PnfcD2Kmkkh';
+}
