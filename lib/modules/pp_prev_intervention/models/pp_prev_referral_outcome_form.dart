b9f3adcfc188e0960d39a47c6225ad57cdea0d66
megamind <echingalo@gmail.com>
2023-03-14 13:43:20 +0300

    feat: added pp prev referral forms

---
+++ lib/modules/pp_prev_intervention/models/pp_prev_referral_outcome_form.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:kb_mobile_app/models/form_section.dart';
+import 'package:kb_mobile_app/models/input_field.dart';
+import 'package:kb_mobile_app/models/input_field_option.dart';
+
+class PpPrevReferralOutcomeForm {
+  static List<String> getMandatoryField() {
+    return [
+      'hXyqgOWZ17b',
+    ];
+  }
+
+  static List<FormSection> getFormSections() {
+    return [
+      FormSection(
+        name: "Referral outcome form",
+        color: const Color(0xFF9B2BAE),
+        inputFields: [
+          InputField(
+            id: 'hXyqgOWZ17b',
+            name: 'Referral service provided?',
+            valueType: 'BOOLEAN',
+            inputColor: const Color(0xFF9B2BAE),
+            labelColor: const Color(0xFF737373),
+          ),
+          InputField(
+            id: 'lvT9gfpHIlT',
+            name: 'Date service was provided',
+            valueType: 'DATE',
+            inputColor: const Color(0xFF9B2BAE),
+            labelColor: const Color(0xFF737373),
+          ),
+          InputField(
+            id: 'gEjigBuBTmh',
+            name: 'Reasons for rejecting/declining service(s) offered',
+            valueType: 'TEXT',
+            inputColor: const Color(0xFF9B2BAE),
+            labelColor: const Color(0xFF737373),
+            options: [
+              InputFieldOption(
+                code: 'Religion/Culture',
+                name: 'Religion/Culture',
+              ),
+              InputFieldOption(
+                code: 'Service already provided',
+                name: 'Service already provided (by other provider)',
+              ),
+              InputFieldOption(
+                code: 'NotReady',
+                name: 'Not Ready',
+              ),
+              InputFieldOption(
+                code: 'HealthConcerns',
+                name: 'Health Concerns',
+              ),
+              InputFieldOption(
+                code: 'AccessIssues',
+                name: 'Access Issues (resources, time)',
+              ),
+              InputFieldOption(
+                code: 'NotInterested',
+                name: 'Not Interested',
+              ),
+              InputFieldOption(
+                code: 'Other(s)',
+                name: 'Other(s)',
+              ),
+            ],
+          ),
+          InputField(
+            id: 'oTTL6vEpKok',
+            name: 'Specify other',
+            inputColor: const Color(0xFF9B2BAE),
+            labelColor: const Color(0xFF737373),
+            valueType: 'LONG_TEXT',
+          ),
+          InputField(
+            id: 'Ep3atnNQGTY',
+            name: 'Follow-up required',
+            inputColor: const Color(0xFF9B2BAE),
+            labelColor: const Color(0xFF737373),
+            valueType: 'BOOLEAN',
+          ),
+          InputField(
+            id: 'DPf5mUDoZMy',
+            name: 'Follow-up date',
+            inputColor: const Color(0xFF9B2BAE),
+            labelColor: const Color(0xFF737373),
+            valueType: 'DATE',
+          ),
+          InputField(
+            id: 'LcG4J82PM4Z',
+            name: 'Comments or next steps',
+            inputColor: const Color(0xFF9B2BAE),
+            labelColor: const Color(0xFF737373),
+            valueType: 'LONG_TEXT',
+          ),
+        ],
+      ),
+    ];
+  }
+}
