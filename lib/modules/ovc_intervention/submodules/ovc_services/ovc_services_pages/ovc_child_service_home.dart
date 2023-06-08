e49cedef16d4d3254153d067aa0d885474003365
megamind <echingalo@gmail.com>
2023-06-08 18:25:18 +0300

    feat(ovc_module): added condition for showing CLHIV ART Card service
    only showed the service for children with HIV status Positive

--- lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/ovc_child_service_home.dart
+++ lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/ovc_child_service_home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kb_mobile_app/app_state/enrollment_service_form_state/service_event_data_state.dart';
 import 'package:kb_mobile_app/app_state/intervention_card_state/intervention_card_state.dart';
+import 'package:kb_mobile_app/app_state/ovc_intervention_list_state/ovc_household_current_selection_state.dart';
 import 'package:kb_mobile_app/core/components/intervention_bottom_navigation/intervention_bottom_navigation_bar_container.dart';
 import 'package:kb_mobile_app/core/components/circular_process_loader.dart';
 import 'package:kb_mobile_app/core/components/sub_page_app_bar.dart';
@@ -120,45 +121,66 @@ class _OvcChildServiceHomeState extends State<OvcChildServiceHome> {
                     : Container(
                         margin: const EdgeInsets.symmetric(
                             vertical: 16.0, horizontal: 13.0),
-                        child: GridView.count(
-                          crossAxisCount: 2,
-                          primary: false,
-                          mainAxisSpacing: 10.0,
-                          crossAxisSpacing: 10.0,
-                          shrinkWrap: true,
-                          children: ovcChildServiceHomeCards.map(
-                            (OvcChildServiceHomeConstant
-                                ovcChildServiceHomeCard) {
-                              int countValue =
-                                  getCountValueForOvcServiceChildCard(
-                                ovcChildServiceHomeCard,
-                                eventListByProgramStage,
-                              );
-                              return Container(
-                                alignment: Alignment.center,
-                                margin: const EdgeInsets.all(5.0),
-                                child: InkWell(
-                                  child: OvcServiceChildCard(
-                                    ovcChildServiceHomeCard:
-                                        ovcChildServiceHomeCard,
-                                    countValue: countValue.toString(),
-                                  ),
-                                  onTap: () => ovcChildServiceHomeCard.id ==
-                                          'assessment'
-                                      ? onOpenChildAssessment(context)
-                                      : ovcChildServiceHomeCard.id == 'casePlan'
-                                          ? onOpenChildCasePlan(context)
+                        child: Consumer<OvcHouseholdCurrentSelectionState>(
+                          builder: (
+                            context,
+                            ovcHouseholdCurrentSelectionState,
+                            child,
+                          ) {
+                            var currentOvcSelection =
+                                ovcHouseholdCurrentSelectionState
+                                    .currentOvcHouseholdChild;
+                            return GridView.count(
+                              crossAxisCount: 2,
+                              primary: false,
+                              mainAxisSpacing: 10.0,
+                              crossAxisSpacing: 10.0,
+                              shrinkWrap: true,
+                              children: ovcChildServiceHomeCards
+                                  .where((ovcChildServiceCard) =>
+                                      // Condition to display CLHIV ART Card service only for HIV Positive children
+                                      ovcChildServiceCard.id != 'clhiv_art' ||
+                                      (ovcChildServiceCard.id == 'clhiv_art' &&
+                                          currentOvcSelection?.hivStatus ==
+                                              'Positive'))
+                                  .map(
+                                (OvcChildServiceHomeConstant
+                                    ovcChildServiceHomeCard) {
+                                  int countValue =
+                                      getCountValueForOvcServiceChildCard(
+                                    ovcChildServiceHomeCard,
+                                    eventListByProgramStage,
+                                  );
+                                  return Container(
+                                    alignment: Alignment.center,
+                                    margin: const EdgeInsets.all(5.0),
+                                    child: InkWell(
+                                      child: OvcServiceChildCard(
+                                        ovcChildServiceHomeCard:
+                                            ovcChildServiceHomeCard,
+                                        countValue: countValue.toString(),
+                                      ),
+                                      onTap: () => ovcChildServiceHomeCard.id ==
+                                              'assessment'
+                                          ? onOpenChildAssessment(context)
                                           : ovcChildServiceHomeCard.id ==
-                                                  'services'
-                                              ? onOpenChildService(context)
+                                                  'casePlan'
+                                              ? onOpenChildCasePlan(context)
                                               : ovcChildServiceHomeCard.id ==
-                                                      'monitor'
-                                                  ? onOpenChildMonitor(context)
-                                                  : null,
-                                ),
-                              );
-                            },
-                          ).toList(),
+                                                      'services'
+                                                  ? onOpenChildService(context)
+                                                  : ovcChildServiceHomeCard
+                                                              .id ==
+                                                          'monitor'
+                                                      ? onOpenChildMonitor(
+                                                          context)
+                                                      : null,
+                                    ),
+                                  );
+                                },
+                              ).toList(),
+                            );
+                          },
                         ),
                       );
               },
